93c48d94ce0273ab01b78b68cbc08432ccd814b4
Glitch (hello-express) <none>
2020-08-17 11:01:46 +0000

    🏙💁 Checkpoint ./server.js:19971311/3583 ./package.json:19971311/6935 ./handlers/commands.js:19971311/1621 ./handlers/xp.js:19971311/1008 ./commands/info/avatar.js:19971311/1005 ./commands/info/dm.js:19971311/811 ./commands/info/help.js:19971311/1713 ./commands/info/level.js:19971311/1070

---
+++ commands/info/avatar.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  name: "avatar",
+  description: "Get your own or someone else's avatar",
+  usage: "[user mention]",
+  category: "info",
+  run: async (bot, message, args) => {
+    let Embed = new MessageEmbed();
+    let roles = [];
+    if (!message.mentions.users.first()) {
+      message.member.roles.cache.forEach((role) => {
+        roles.push(role.name);
+      });
+      Embed.setTitle(`Your avatar!`);
+      Embed.setThumbnail(message.author.displayAvatarURL());
+      Embed.setColor(`BLUE`);
+      Embed.setDescription
+    return message.channel.send(Embed);
+    } else {
+      let User = message.mentions.members.first();
+      User.roles.cache.forEach((role) => {
+        roles.push(role.name);
+      });
+      Embed.setTitle(`${bot.users.cache.get(User.id).tag}'s avatar!`);
+      Embed.setThumbnail(bot.users.cache.get(User.id).displayAvatarURL());
+      Embed.setColor(`RANDOM`);
+      
+      return message.channel.send(Embed);
+    }
+  },
+};
--- server.js
+++ server.js
@@ -1,34 +1,78 @@
-// server.js
-// where your node app starts
+const { Client, Collection, MessageAttachment } = require("discord.js");
+const { config } = require("dotenv");
+const { default_prefix, token } = require("./config.json");
+const db = require("quick.db")
+const fs = require("fs");
+const discord = require("discord.js");
+const { CanvasSenpai } = require("canvas-senpai")
+const canva = new CanvasSenpai();
+const { addexp } = require("./handlers/xp.js");
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+const client = new Client({
+  disableEveryone: true
+});
+// Collections
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands");
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+// Run the command loader
+["command"].forEach(handler => {
+  //some error here
+  require(`./handlers/${handler}`)(client);
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+client.on("ready", () => {
+  function randomStatus() {
+    let status = [`${client.guilds.cache.size} servers!`, "type:!help for command" , "Created By BUDDY"] // You can change it whatever you want.
+    let rstatus = Math.floor(Math.random() * status.length);
+    client.user.setActivity(status[rstatus], {type: "WATCHING"});
+  }; setInterval(randomStatus, 3) // Time in ms. 3000ms = 3 seconds. Min: 20 seconds, to avoid ratelimit.
+  
+  console.log('Online.')
+  });
+//
+client.on("message", async message => {
+if(!message.guild) return;
+  let prefix = db.get(`prefix_${message.guild.id}`)
+  if(prefix === null) prefix = default_prefix;
+  if(!message.content.startsWith(prefix)) return;
+  
+  //YOUR CODE
+
+  // If message.member is uncached, cache it.
+  if (!message.member)
+    message.member = await message.guild.fetchMember(message);
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
+  if (cmd.length === 0) return;
+ 
+  // Get the command
+  let command = client.commands.get(cmd);
+  // If none is found, try to find it y alias
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+  // If a command is finally found 
+  if(command) {  
+    command.run(client, message, args)
+  return addexp(message)
+    }
+
+  })  
+client.on("guildMemberAdd", async member => { //usage of welcome event
+  let chx = db.get(`welchannel_${member.guild.id}`);
+  //defining var
+    let data = await canva.welcome(member, { link: "https://wallpapercave.com/wp/wp6081431.jpg", blur: false  })
+    
+    const attachment = new discord.MessageAttachment(
+      data,
+      "welcome-image.png"
+    );
+ client.channels.cache.get(chx).send(`Welcome to the server, ${member.user.username}!`,
+      attachment) //get channel and send embed
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+    client.login(token);
