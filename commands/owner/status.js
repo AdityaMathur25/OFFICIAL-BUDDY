c0dddf58c69d4533682692a80ac95bd9da4f5d03
Glitch (hello-express) <none>
2020-08-17 18:10:19 +0000

    🦄👍 Checkpoint ./commands/owner/status.js:19971311/413

--- commands/owner/status.js
+++ commands/owner/status.js
@@ -7,8 +7,6 @@ module.exports = {
   usage: "status <here>",
   category: "owner",
   run: async (client, message, args) => {
-    let embed = new discord.Messageembed()
-    .
     //OWNER ONLY COMMAND
     if(!message.author.id === "480285300484997122") {
       return message.channel.send("This command can only be used by bot owner")
@@ -21,6 +19,11 @@ module.exports = {
  db.set(`status`, args.join(" "))
    await message.channel.send("Updated the bot status")
     process.exit(1);
-    
+ 
+    let embed = new discord.MessageEmbed()
+    .setcolor("GREEN")
+    .setfooter(client.user.displayavtarURL)
+   
+    message.channel.send(embed)
   }
 }
