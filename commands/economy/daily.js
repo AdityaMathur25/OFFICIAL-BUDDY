4d573956177cc71c52023e41891f11db58b0adda
Glitch (deep-political-damselfly) <none>
2020-08-26 06:49:35 +0000

    😻😼 Checkpoint ./commands/economy/daily.js:19971311/28 ./package.json:19971311/1902 ./commands/info/covid.js:19971311/2254

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -8,7 +8,7 @@ module.exports = {
 
     async run (client, message, args) {
         let user = message.author;
-        let timeout = 10;
+        let timeout = 86400000;
         let amount = 1000;
 
         let daily = await db.fetch(`daily_${message.guild.id}_${user.id}`);
