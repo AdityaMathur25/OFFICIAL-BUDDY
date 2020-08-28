cf99e8a4c4fb1148514fff381a2d12e7ce4a1f56
Glitch (deep-political-damselfly) <none>
2020-08-28 09:36:14 +0000

    🐃🏞 Checkpoint ./server.js:19971311/644:21497790/1337

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ const discord = require("discord.js");
 const { CanvasSenpai } = require("canvas-senpai")
 const canva = new CanvasSenpai();
 const { addexp } = require("./handlers/xp.js");
-const URL = "";
+
+
+
+let random = Math.floor(Math.random() * 2);
 //for image ?
 const client = new Client({
   disableEveryone: true
-});
+})
+// for not taging everyone.
 // Collections
 client.commands = new Collection();
 client.aliases = new Collection();
@@ -64,9 +68,10 @@ client.on("guildMemberAdd", async member => { //usage of welcome event
   let chx = db.get(`welchannel_${member.guild.id}`);
   //defining var
  if(!chx) return; 
-  var images = ["https://wallpapercave.com/wp/wp6081521.jpg", "https://wallpapercave.com/wp/wp5128415.jpgcimage", "https://wallpapercave.com/wp/wp5128398.jpg", "https://wallpapercave.com/wp/wp5700007.jpg", "https://wallpapercave.com/wp/wp5243211.jpg", "https://www.google.com/search?q=anime+banner&client=ms-android-vivo&sxsrf=ALeKk03xqUuHfZGJlsbrL-yPfiQrucZNLQ:1598531269038&source=lnms&tbm=isch#", "" ]
-var image = Math.floor((Math.random * images.length))
-    let data = await canva.welcome(member, { link:  `${images[image]}`} )
+  //u not define at random for image ?
+  var images = ["https://wallpapercave.com/wp/wp6081521.jpg", "https://wallpapercave.com/wp/wp5128415.jpgcimage"]
+let random = Math.floor(Math.random() * 2);
+    let data = await canva.welcome(member, { link:  `${images[random]}`} )
     const attachment = new discord.MessageAttachment(
       data,
       "welcome-image.png"
@@ -78,8 +83,8 @@ var image = Math.floor((Math.random * images.length))
 client.on("guildMemberRemove", async member => { //usage of welcome event
   let chx = db.get(`leavchannel_${member.guild.id}`);
   //defining var
-    let data = await canva.welcome(member, { link: "https://wallpapercave.com/wp/wp6081431.jpg", blur: false  })
-    
+    let data = await canva.welcome(member, { link: random , blur: false  })
+    //its leave .
     const attachment = new discord.MessageAttachment(
       data,
       "leave-image.png"
