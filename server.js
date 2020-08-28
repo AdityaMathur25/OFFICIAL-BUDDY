80c6de1da0960e7c4ab2a908186bb766b855e21d
Glitch (deep-political-damselfly) <none>
2020-08-28 09:24:14 +0000

    💒☂️ Checkpoint ./server.js:19971311/16:21497790/15

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const discord = require("discord.js");
 const { CanvasSenpai } = require("canvas-senpai")
 const canva = new CanvasSenpai();
 const { addexp } = require("./handlers/xp.js");
-
+const URL = "";
+//for image ?
 const client = new Client({
   disableEveryone: true
 });
