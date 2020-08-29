15602e62d6d52d3caceb6ff8c47fd8f10e19eb3d
Glitch (deep-political-damselfly) <none>
2020-08-29 09:01:03 +0000

    🏝👩‍👩‍👦 Checkpoint ./server.js:8483431/10014:20536785/24

--- server.js
+++ server.js
@@ -32,112 +32,117 @@ client.on("ready", () => {
 });
 //
 
-client.on("message",async message => {
-if(message.author.client) return;
-
-if(message.content.indexof(default_prefix) !== 0) return;
-const args = message.content
-.slice(default_prefix.length)
-.tirm()
-.split(/ +/g);
-
-const command = args.shift().toLowerCase();
-
-//where are u ? 
-
- 
-let guildArray = client.guilds.array();
-client.on("guildCreate", guild => {
-
-  let join = new discord.MessageEmbed()
-    .setColor("#00FFFF")
-    .setTitle("New Server Joined")
-    .addField("Server ID :", guild.id)
-    .addField("Server Members :", guild.memberCount)
-    .addField("Server Name :", guild.name)
-    .setThumbnail(guild.iconURL())
-    .addField("Server Owner :", guild.owner.user.tag)
-    .addField("VERIFICATION LEVEL :", `${vLevel}`)
-client.channels.find(r => r.id == "730641391457140809").send(join)
-message.channel.send(join);
-console.log("New Server Joined" +guild.name )
-  
-})
-client.on("guildDelete", guild => {
-
-  let join = new discord.MessageEmbed()
-    .setColor("#00FFFF")
-    .setTitle("New Server Joined")
-    .addField("Server ID :", guild.id)
-    .addField("Server Members :", guild.memberCount)
-    .addField("Server Name :", guild.name)
-    .setThumbnail(guild.iconURL())
-    .addField("Server Owner :", guild.owner.user.tag)
-    .addField("VERIFICATION LEVEL :", `${vLevel}`)
-client.channels.find(r => r.id == "730641391457140809").send(join)
-message.channel.send(join);
-console.log("New Server left" +guild.name )
-  
-})
-
 client.on("message", async message => {
-  if (!message.guild) return;
-  let prefix = db.get(`prefix_${message.guild.id}`);
-  if (prefix === null) prefix = default_prefix;
-  if (!message.content.startsWith(prefix)) return;
+  if (message.author.client) return;
 
-  //YOUR CODE
-
-  // If message.member is uncached, cache it.
-  if (!message.member)
-    message.member = await message.guild.fetchMember(message);
+  if (message.content.indexof(default_prefix) !== 0) return;
   const args = message.content
-    .slice(prefix.length)
-    .trim()
+    .slice(default_prefix.length)
+    .tirm()
     .split(/ +/g);
-  const cmd = args.shift().toLowerCase();
 
-  if (cmd.length === 0) return;
+  const command = args.shift().toLowerCase();
 
-  // Get the command
-  let command = client.commands.get(cmd);
-  // If none is found, try to find it y alias
-  if (!command) command = client.commands.get(client.aliases.get(cmd));
-  // If a command is finally found
-  if (command) {
-    command.run(client, message, args);
-    return addexp(message);
-  }
-});
+  //where are u ?
+  client.on("guildCreate", guild => {
+    let defaultChannel = "";
+    guild.channels.cache.forEach(channel => {
+      if (channel.type == "text" && defaultChannel == "") {
+        if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+          defaultChannel = channel;
+        }
+        defaultChannel.send("Hello, Im a Bot!");
+      }
+    });
 
-client.on("guildMemberAdd", async member => {
-  //usage of welcome event
-  let chx = db.get(`welchannel_${member.guild.id}`);
-  //defining var
-  if (!chx) return;
-  //u not define at random for image ?
-  var images = [
-    "https://wallpapercave.com/wp/wp6081521.jpg",
-    "https://wallpapercave.com/wp/wp5128415.jpg",
-    "https://wallpapercave.com/wp/wp5128398.jpg",
-    "https://wallpapercave.com/wp/wp5700007.jpg",
-    "https://wallpapercave.com/wp/wp5243211.jpg"
-  ];
-  let random = Math.floor(Math.random() * 4); //no i dont want 4 image 1 omly
-  let data = await canva.welcome(member, { link: `${images[random]}` });
-  const attachment = new discord.MessageAttachment(data, "welcome-image.png");
-  client.channels.cache
-    .get(chx)
-    .send(`Welcome to the server, ${member.user.username}!`, attachment); //get channel and send embed
-});
+    let guildArray = client.guilds.array();
+    client.on("guildCreate", guild => {
+      let join = new discord.MessageEmbed()
+        .setColor("#00FFFF")
+        .setTitle("New Server Joined")
+        .addField("Server ID :", guild.id)
+        .addField("Server Members :", guild.memberCount)
+        .addField("Server Name :", guild.name)
+        .setThumbnail(guild.iconURL())
+        .addField("Server Owner :", guild.owner.user.tag)
+        .addField("VERIFICATION LEVEL :", "${vLevel}");
+      client.channels.find(r => r.id == "730844022020767815").send(join);
+      message.channel.send(join);
+      console.log("New Server Joined" + guild.name);
+    });
+    client.on("guildDelete", guild => {
+      let join = new discord.MessageEmbed()
+        .setColor("#00FFFF")
+        .setTitle("New Server Joined")
+        .addField("Server ID :", guild.id)
+        .addField("Server Members :", guild.memberCount)
+        .addField("Server Name :", guild.name)
+        .setThumbnail(guild.iconURL())
+        .addField("Server Owner :", guild.owner.user.tag)
+        .addField("VERIFICATION LEVEL :", "${vLevel}");
+      client.channels.find(r => r.id == "730844022020767815").send(join);
+      message.channel.send(join);
+      console.log("New Server left" + guild.name);
+    });
 
+    client.on("message", async message => {
+      if (!message.guild) return;
+      let prefix = db.get(`prefix_${message.guild.id}`);
+      if (prefix === null) prefix = default_prefix;
+      if (!message.content.startsWith(prefix)) return;
 
+      //YOUR CODE
 
-//define message lol
+      // If message.member is uncached, cache it.
+      if (!message.member)
+        message.member = await message.guild.fetchMember(message);
+      const args = message.content
+        .slice(prefix.length)
+        .trim()
+        .split(/ +/g);
+      const cmd = args.shift().toLowerCase();
 
+      if (cmd.length === 0) return;
 
+      // Get the command
+      let command = client.commands.get(cmd);
+      // If none is found, try to find it y alias
+      if (!command) command = client.commands.get(client.aliases.get(cmd));
+      // If a command is finally found
+      if (command) {
+        command.run(client, message, args);
+        return addexp(message);
+      }
+    });
 
+    client.on("guildMemberAdd", async member => {
+      //usage of welcome event
+      let chx = db.get(`welchannel_${member.guild.id}`);
+      //defining var
+      if (!chx) return;
+      //u not define at random for image ?
+      var images = [
+        "https://wallpapercave.com/wp/wp6081521.jpg",
+        "https://wallpapercave.com/wp/wp5128415.jpg",
+        "https://wallpapercave.com/wp/wp5128398.jpg",
+        "https://wallpapercave.com/wp/wp5700007.jpg",
+        "https://wallpapercave.com/wp/wp5243211.jpg"
+      ];
+      let random = Math.floor(Math.random() * 4); //no i dont want 4 image 1 omly
+      let data = await canva.welcome(member, { link: `${images[random]}` });
+      const attachment = new discord.MessageAttachment(
+        data,
+        "welcome-image.png"
+      );
+      client.channels.cache
+        .get(chx)
+        .send(`Welcome to the server, ${member.user.username}!`, attachment); //get channel and send embed
+    });
 
+    //define message lol
+
+    // wait  dont do anything
+  });
 });
 //what is used for v12 in ? for guildCreate i dont no that why
 client.login(token);
