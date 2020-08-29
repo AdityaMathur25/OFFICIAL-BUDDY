7a4da95f91fe771fb87e037da543a15dae4d8be9
Glitch (deep-political-damselfly) <none>
2020-08-29 09:34:43 +0000

    💗🎳 Checkpoint ./server.js:8483431/8914:20536785/116

--- server.js
+++ server.js
@@ -32,68 +32,45 @@ client.on("ready", () => {
 });
 //
 
-
-  //where are u ?
-  
-
-    wtf
-
-    
-      if (cmd.length === 0) return;
-
-      // Get the command
-      let command = client.commands.get(cmd);
-      // If none is found, try to find it y alias
-      if (!command) command = client.commands.get(client.aliases.get(cmd));
-      // If a command is finally found
-      if (command) {
-        command.run(client, message, args);
-        return addexp(message);
-      }
-    });
-
-    client.on("guildMemberAdd", async member => {
-      //usage of welcome event
-      let chx = db.get(`welchannel_${member.guild.id}`);
-      //defining var
-      if (!chx) return;
-      //u not define at random for image ?
-      var images = [
-        "https://wallpapercave.com/wp/wp6081521.jpg",
-        "https://wallpapercave.com/wp/wp5128415.jpg",
-        "https://wallpapercave.com/wp/wp5128398.jpg",
-        "https://wallpapercave.com/wp/wp5700007.jpg",
-        "https://wallpapercave.com/wp/wp5243211.jpg"
-      ];
-      let random = Math.floor(Math.random() * 4); //no i dont want 4 image 1 omly
-      let data = await canva.welcome(member, { link: `${images[random]}` });
-      const attachment = new discord.MessageAttachment(
-        data,
-        "welcome-image.png"
-      );
-      client.channels.cache
-        .get(chx)
-        .send(`Welcome to the server, ${member.user.username}!`, attachment); //get channel and send embed
-    });
+client.on("guildMemberAdd", async member => {
+  //usage of welcome event
+  let chx = db.get(`welchannel_${member.guild.id}`);
+  //defining var
+  if (!chx) return;
+  //u not define at random for image ?
+  var images = [
+    "https://wallpapercave.com/wp/wp6081521.jpg",
+    "https://wallpapercave.com/wp/wp5128415.jpg",
+    "https://wallpapercave.com/wp/wp5128398.jpg",
+    "https://wallpapercave.com/wp/wp5700007.jpg",
+    "https://wallpapercave.com/wp/wp5243211.jpg"
+  ];
+  let random = Math.floor(Math.random() * 4); //no i dont want 4 image 1 omly
+  let data = await canva.welcome(member, { link: `${images[random]}` });
+  const attachment = new discord.MessageAttachment(data, "welcome-image.png");
+  client.channels.cache
+    .get(chx)
+    .send(`Welcome to the server, ${member.user.username}!`, attachment); //get channel and send embed
+});
 
 client.on("message", async message => {
-      if (!message.guild) return;
-      let prefix = db.get(`prefix_${message.guild.id}`);
-      if (prefix === null) prefix = default_prefix;
-      if (!message.content.startsWith(prefix)) return;
+  if (!message.guild) return;
+  let prefix = db.get(`prefix_${message.guild.id}`);
+  if (prefix === null) prefix = default_prefix;
+  if (!message.content.startsWith(prefix)) return;
 
-      //YOUR CODE
+  //YOUR CODE
 
-      // If message.member is uncached, cache it.
-      if (!message.member)
-        message.member = await message.guild.fetchMember(message);
-      const args = message.content
-        .slice(prefix.length)
-        .trim()
-        .split(/ +/g);
-      const cmd = args.shift().toLowerCase();
+  // If message.member is uncached, cache it.
+  if (!message.member)
+    message.member = await message.guild.fetchMember(message);
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  const cmd = args.shift().toLowerCase();
 
-switch (message.guild.verificationLevel) {
+  switch (message.guild.verificationLevel) {
     case 0:
       var vLevel = "None";
       break;
@@ -106,52 +83,53 @@ switch (message.guild.verificationLevel) {
     case 3:
       var vLevel = "(╯°□°）╯︵ ┻━┻";
       break;
- case 4:
+    case 4:
       var vLevel = "┻━┻︵  (°□°）/ ︵ ┻━┻";
       break;
   }
-switch (message.guild.explicitContentFilter) {
-  case 0: 
-var cFilter = "Dont Scan any messages";
-break;
- case 1: 
-var cFilter = "Scan messages from members without a role";
-break;
- case 2: 
-var cFilter = "Scan messages sent by all members";
-break;
-}
+  switch (message.guild.explicitContentFilter) {
+    case 0:
+      var cFilter = "Dont Scan any messages";
+      break;
+    case 1:
+      var cFilter = "Scan messages from members without a role";
+      break;
+    case 2:
+      var cFilter = "Scan messages sent by all members";
+      break;
+  }
 
-client.on("guildCreate", guild => {
-  let join = new discord.MessageEmbed()
-    .setColor(0xff0000)
-    .setTitle("Server Joined")
-    .addField("Server Name :", guild.name)
-    .addField("Server Owner :", guild.owner.user.tag)
-    .setThumbnail(guild.iconURL)
-    .addField("Server ID :", guild.id)
-    .addField("Server Members :", guild.memberCount)
- client.channels.cache.get("748936869022007376").send(join)
-  console.log("Joined a new guild: " + guild.name);
+  client.on("guildCreate", guild => {
+    let join = new discord.MessageEmbed()
+      .setColor(0x00ffff)
+      .setTitle("Server Joined")
+      .addField("Server Name :", guild.name)
+      .addField("Server Owner :", guild.owner.user.tag)
+      .setThumbnail(guild.iconURL)
+      .addField("Server ID :", guild.id)
+      .addField("Server Members :", guild.memberCount)
+.addField
+"VerificationLevel", `${vLevel}`    
+client.channels.cache.get("748936869022007376").send(join);
+    console.log("Joined a new guild: " + guild.name);
+  });
+  //mention the guild owner not tag. LOL YOU CANT MENTION THEN BZ THEY ARE NOT IN YOUR SERVER
+
+  client.on("guildDelete", guild => {
+    let join = new discord.MessageEmbed()
+        .setColor(0x00ffff)    .setTitle("Server Joined")
+      .addField("Server Name :", guild.name)
+      .addField("Server Owner :", guild.owner.user.tag)
+      .setThumbnail(guild.iconURL)
+      .addField("Server ID :", guild.id)
+      .addField("Server Members :", guild.memberCount);
+    client.channels.cache.get("748936869022007376").send(join);
+    console.log("Left Server: " + guild.name);
+  });
+  //define message lol
 });
-//mention the guild owner not tag. LOL YOU CANT MENTION THEN BZ THEY ARE NOT IN YOUR SERVER 
+// wait  dont do anything
 
-client.on("guildDelete", guild => {
-  let join = new discord.MessageEmbed()
-    .setColor(0xff0000)
-    .setTitle("Server Joined")
-    .addField("Server Name :", guild.name)
-    .addField("Server Owner :", guild.owner.user.tag)
-    .setThumbnail(guild.iconURL)
-    .addField("Server ID :", guild.id)
-    .addField("Server Members :", guild.memberCount)
-   client.channels.cache.get("748936869022007376").send(join)
-  console.log("Left Server: " + guild.name);
-});
-    //define message lol
-
-    // wait  dont do anything
-  
 //what is used for v12 in ? for guildCreate i dont no that why
 client.login(token);
 //not get.
