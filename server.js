47c41182fbabe848f34e34e80777fa1eba1a066c
Glitch (feather-expensive-bus) <none>
2020-08-30 05:18:46 +0000

    Rewound to commit 7a1d3f2680e340225b357579a6d97cd26a3f10bc + 0 edits

--- server.js
+++ server.js
@@ -28,9 +28,38 @@ client.categories = fs.readdirSync("./commands");
 client.on("ready", () => {
   client.user.setActivity(db.get(`status`), { type: "WATCHING" });
 
-  console.log(client.user.tag);
+  console.log("ready as badass");
 });
 //
+client.on("message", async message => {
+  if (!message.guild) return;
+  let prefix = db.get(`prefix_${message.guild.id}`);
+  if (prefix === null) prefix = default_prefix;
+  if (!message.content.startsWith(prefix)) return;
+
+  //YOUR CODE
+
+  // If message.member is uncached, cache it.
+  if (!message.member)
+    message.member = await message.guild.fetchMember(message);
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
+  if (cmd.length === 0) return;
+
+  // Get the command
+  let command = client.commands.get(cmd);
+  // If none is found, try to find it y alias
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+  // If a command is finally found
+  if (command) {
+    command.run(client, message, args);
+    return addexp(message);
+  }
+});
 
 client.on("guildMemberAdd", async member => {
   //usage of welcome event
@@ -53,54 +82,77 @@ client.on("guildMemberAdd", async member => {
     .send(`Welcome to the server, ${member.user.username}!`, attachment); //get channel and send embed
 });
 
-client.on("message", async message => {
-  if (!message.guild) return;
-  let prefix = db.get(`prefix_${message.guild.id}`);
-  if (prefix === null) prefix = default_prefix;
-  if (!message.content.startsWith(prefix)) return;
-
-  //YOUR CODE
-
-  // If message.member is uncached, cache it.
-  if (!message.member)
-    message.member = await message.guild.fetchMember(message);
-  const args = message.content
-    .slice(prefix.length)
-    .trim()
-    .split(/ +/g);
-  const cmd = args.shift().toLowerCase();
-  
-
-  client.on("guildCreate", guild => {
-    let join = new discord.MessageEmbed()
-      .setColor(0x00ffff)
-      .setTitle("Server Joined")
-      .addField("Server Name :", guild.name)
-      .addField("Server Owner :", guild.owner.user.tag)
-      .setThumbnail(guild.iconURL)
-      .addField("Server ID :", guild.id)
-      .addField("Server Members :", guild.memberCount)
-.addField("VERIFICATION LEVEL :", guild.VerificationLevel)   
-client.channels.cache.get("748936869022007376").send(join);
-    console.log("Joined a new guild: " + guild.name);
-  });
-  //mention the guild owner not tag. LOL YOU CANT MENTION THEN BZ THEY ARE NOT IN YOUR SERVER
-
-  client.on("guildDelete", guild => {
-    let join = new discord.MessageEmbed()
-        .setColor(0x00ffff)    .setTitle("Server Joined")
-      .addField("Server Name :", guild.name)
-      .addField("Server Owner :", guild.owner.user.tag)
-      .setThumbnail(guild.iconURL)
-      .addField("Server ID :", guild.id)
-      .addField("Server Members :", guild.memberCount);
-    client.channels.cache.get("748936869022007376").send(join);
-    console.log("Left Server: " + guild.name);
-  });
-  //define message lol
+client.on("guildMemberRemove", async member => {
+  //usage of welcome event
+  let chx = db.get(`leavchannel_${member.guild.id}`);
+  //defining var
+  let data = await canva.welcome(member, { link: random });
+  //its leave .
+  const attachment = new discord.MessageAttachment(data, "leave-image.png");
+  client.channels.cache
+    .get(chx)
+    .send(` ${member.user.username} JUST LEFT THE SERVER !`, attachment); //get channel and send embed
 });
-// wait  dont do anything
 
-//what is used for v12 in ? for guildCreate i dont no that why
+
+//define message lol
+
+
+client.on("message",async message => {
+if(message.author.client) return;
+
+if(message.content.indexof(default_prefix) !== 0) return;
+const args = message.content
+.slice(default_prefix.length)
+.tirm()
+.split(/ +/g);
+
+const command = args.shift().toLowerCase();
+
+
+client.on("guildCreate", guild => {
+  switch (message.guild.verificationLevel) {
+    case 0:
+      var vLevel = "None";
+      break;
+    case 1:
+      var vLevel = "Low";
+      break;
+    case 2:
+      var vLevel = "Medium";
+      break;
+    case 3:
+      var vLevel = "(╯°□°）╯︵ ┻━┻";
+      break;
+ case 4:
+      var vLevel = "┻━┻︵  (°□°）/ ︵ ┻━┻";
+      break;
+  }
+
+switch (message.guild.explicitContentFilter) {
+  case 0: 
+var cFilter = "Dont Scan any messages";
+break;
+ case 1: 
+var cFilter = "Scan messages from members without a role";
+break;
+ case 2: 
+var cFilter = "Scan messages sent by all members";
+break;
+}
+
+  let join = new discord.MessageEmbed()
+    .setColor("#00FFFF")
+    .setTitle("New Server Joined")
+    .addField("Server ID :", guild.id)
+    .addField("Server Members :", guild.memberCount)
+    .addField("Server Name :", guild.name)
+    .setThumbnail(guild.iconURL())
+    .addField("Server Owner :", guild.owner.user.tag)
+    .addField("VERIFICATION LEVEL :", `${vLevel}`)
+client.channels.find(r => r.id == "748936869022007376").send(join)
+message.channel.send(join);
+  
+})})
 client.login(token);
-//not get.
+
