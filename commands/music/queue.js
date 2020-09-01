fa7158db7714669a48e96833db881cdba61e04a4
Glitch (feather-expensive-bus) <none>
2020-09-01 13:11:54 +0000

    🍎🚉 Checkpoint ./commands/music/queue.js:20536785/609

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,17 +1,23 @@
-  const MessageEmbed = require("discord.js")
+  const { MessageEmbed }= require("discord.js")
   module.exports = {
     name: "queue",
     Description:"show playing music",
     category:"music",
 run:  (client, message, args) => {
 const serverQueue = message.client.queue.get(message.guild.id);
-  if (!serverQueue) return message.channel.send("There is nothing playing.");
-  const queue = new MessageEmbed();
-  queue.setAuthor("QUEUE SONGS!", message.author.displayAvatarURL({dynamic: true}))
-  queue.setColor("GREEN")
-  queue.setDescription(`${serverQueue.songs.map((song) => `**-** ${song.title}`).join("\n")}**Now playing:** ${serverQueue.songs[0].title}`)
-  queue.setFooter(`REQUESTED BY  ${message.author.username}`)
-  queue.setTimestamp();
+const NOT = new MessageEmbed();
+  NOT.setTitle(`${message.author}`)
+  NOT.setDescription("THERE IS NOTHING TO PLAY !")
+  NOT.setColor("#00FFFF")
+  NOT.setFooter("CREATED BY BUDDY")
+  if (!serverQueue) 
+    return message.channel.send(NOT);
+  const MUSIC = new MessageEmbed();
+  MUSIC.setAuthor("QUEUE SONGS!", message.author.displayAvatarURL({dynamic: true}))
+  MUSIC.setColor("GREEN")
+  MUSIC.setDescription(`${serverQueue.songs.map((song) => `**-** ${song.title}`).join("\n")}**Now playing:** ${serverQueue.songs[0].title}`)
+  MUSIC.setFooter(`REQUESTED BY  ${message.author.username}`)
+  MUSIC.setTimestamp();
   return message.channel.send(queue);
 }
   }
