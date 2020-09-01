5936147d7d4f108b5d32dd1a8238b484f74b53ac
Glitch (feather-expensive-bus) <none>
2020-09-01 08:33:20 +0000

    🚥📇 Checkpoint ./commands/music/queue.js:20536785/185

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,17 +1,18 @@
   
   module.exports = {
+    name: "queue",
+    Description:"show playing music",
+    category:"music",
 run:  (client, message, args) => {
-
-  const serverQueue = message.client.queue.get(message.guild.id);
+const serverQueue = message.client.queue.get(message.guild.id);
   if (!serverQueue) return message.channel.send("There is nothing playing.");
   const queue = new MessageEmbed();
   queue.setAuthor("QUEUE SONGS!", message.author.displayAvatarURL({dynamic: true}))
   queue.setColor("GREEN")
-  queue.setDescription(`${serverQueue.songs.map((song) => `**-** ${song.title}`).join("\n")}
+  queue.setDescription(`${serverQueue.songs.map((song) => `**-** ${song.title}`).join("\n")}**Now playing:** ${serverQueue.songs[0].title}`)
   queue.setFooter("REQUESTED BY ", message.author.username)
   queue.setTimestamp();
   return message.channel.send(queue);
-**Now playing:** ${serverQueue.songs[0].title}`)
 const MessageEmbed = require("discord.js")
 }
   }
