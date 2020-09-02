6e5c2ccc00909646ab4cf806e8da3ff70d078cf4
Glitch (feather-expensive-bus) <none>
2020-09-02 16:50:12 +0000

    👼📡 Checkpoint ./commands/music/volume.js:20536785/2008

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -1,21 +1,25 @@
-module.exports ={
-  name: "volume",
-  category: "music",
-  description: "set song volume ",
-run: (client, message, args) => {
-  const { channel } = message.member.voice;
-  if (!channel)
-    return message.channel.send(
-      "I'm sorry but you need to be in a voice channel to play music!"
-    );
-  const serverQueue = message.client.queue.get(message.guild.id);
-  if (!serverQueue) return message.channel.send("There is nothing playing.");
-  if (!args[0])
-    return message.channel.send(
-      `The current volume is: **${serverQueue.volume}**`
-    );
-  serverQueue.volume = args[0]; 
-  serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
-  return message.channel.send(`I set the volume to: **${args[0]}**`);
-}
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+
+    name: "volume",
+    description: "To change the server song queue volume",
+    usage: "[volume]",
+    aliases: ["v"],
+  
+
+  run: async function (client, message, args) {
+    const channel = message.member.voice.channel;
+    if (!channel)return message.channel.send("I'm sorry but you need to be in a voice channel to play music!", message.channel);
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (!serverQueue) return message.channel.send("There is nothing playing in this server.", message.channel);
+    if (!args[0])return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
+    serverQueue.volume = args[0]; 
+    serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
+    let xd = new MessageEmbed()
+    .setDescription(`I set the volume to: **${args[0]}`)
+    .setTitle("Server Volume Manager")
+    .setColor("BLUE")
+    return message.channel.send(xd);
+  },
 }
