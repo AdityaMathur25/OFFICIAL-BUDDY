ab3b9b53c310c2df86f1e5e6ddc088d42bf698d2
Glitch (feather-expensive-bus) <none>
2020-09-03 18:14:57 +0000

    🙉🛢 Checkpoint ./commands/info/uptime.js:20536785/570

--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js")
- 
+ const { MessageEmbed, client } = require("discord.js")
 module.exports = {
   name: "uptime",
   description: "Show the bot uptime",
-  category: "",
-  aliases: "",
+  category: "info",
+  aliases: ["up"],
 run: async (bot, message, args) => {
  
     function duration(ms) {
@@ -12,8 +12,14 @@ run: async (bot, message, args) => {
         const min = Math.floor((ms / (1000 * 60)) % 60).toString()
         const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
         const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
-        return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minuts, ${sec.padStart(2, '0')} seconds.`
+        return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')} seconds.`
     }
-    message.channel.send(`Je suis en ligne depuis : ${duration(bot.uptime)}`)
+  const embed = new MessageEmbed();
+  embed.setAuthor( message.author.username)
+  embed.setTitle("I HAVE BEEN ONLINE FOR :")
+  embed.setDescription(`${duration(bot.uptime)}`)
+embed.setColor("GREEN")
+  embed.setFooter(`Requested By ${message.author.username}`)
+    message.channel.send(embed)
 }
 }
