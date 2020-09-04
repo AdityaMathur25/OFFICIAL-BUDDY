37f8319086a7a9a8b8d2560943cf124edfefefaa
Glitch (feather-expensive-bus) <none>
2020-09-04 09:24:32 +0000

    🏋🐢 Checkpoint ./commands/owner/restart.js:20536785/433

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -7,6 +7,6 @@ module.exports = {
             return message.channel.send(`You cannot use this command!`)
         }
         await message.channel.send(`Restarting bot...`)
-        process.exit();
+      process.exit();
     }
-}
+          }
