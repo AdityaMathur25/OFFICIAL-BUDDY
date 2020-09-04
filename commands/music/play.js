a549a13858473a1e7cbb88b0b04d8de8d17fab51
Glitch (feather-expensive-bus) <none>
2020-09-04 05:55:11 +0000

    🍈🍯 Checkpoint ./commands/music/play.js:20536785/314

--- commands/music/play.js
+++ commands/music/play.js
@@ -56,7 +56,7 @@ module.exports = {
       voiceChannel: channel,
       connection: null,
       songs: [],
-      volume: 2,
+      volume: 100,
       playing: true,
     };
     message.client.queue.set(message.guild.id, queueConstruct);
@@ -65,7 +65,7 @@ module.exports = {
     const play = async (song) => {
       const queue = message.client.queue.get(message.guild.id);
       if (!song) {
-        sendError("Leaving the voice channel because I think there are no songs in the queue. If you like the bot stay 24/7 in voice channel go to `commands/play.js` and remove the line number 61\n\nThank you for using my code! [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot)", message.channel)
+        sendError("leaving voice channel")
         queue.voiceChannel.leave();//If you want your bot stay in vc 24/7 remove this line :D
         message.client.queue.delete(message.guild.id);
         return;
