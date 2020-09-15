5bba5880e5900ea7247114579da6a8b706c92e37
Glitch (crystal-panoramic-litter) <none>
2020-09-15 16:37:46 +0000

    🌦💖 Checkpoint ./commands/fun/punch.js:19971311/48 ./commands/fun/meme.js:19971311/4 ./commands/fun/hug.js:19971311/5664

--- commands/fun/punch.js
+++ commands/fun/punch.js
@@ -8,17 +8,17 @@ module.exports ={
   run: async (client, message, args) => {
 
 var list = [
-"https://tenor.com/view/toradora-punch-gif-10769541",
-"https://tenor.com/view/anime-smash-lesbian-punch-wall-gif-4790446",
-"https://tenor.com/view/anime-naruto-punch-fight-gif-12911685",
-"https://tenor.com/view/anime-punch-knockout-wasted-smack-gif-11451829",
-"https://tenor.com/view/die-anime-punch-kill-angry-gif-8932977",
-"https://tenor.com/view/rin243109-blue-exorcist-anime-punch-gif-13785833",
-"https://tenor.com/view/shy-punch-anime-aki-adagaki-gif-12003970",
-"https://tenor.com/view/naruto-sasuke-punch-snicker-anime-gif-7380310",
-"https://tenor.com/view/stay-down-punch-naruto-gif-5211928",
-"https://tenor.com/view/fight-anime-punch-gif-16681456",
-"https://tenor.com/view/azumanga-daioh-azumanga-anime-punch-uppercut-gif-17075114",
+'https://tenor.com/view/toradora-punch-gif-10769541',
+'https://tenor.com/view/anime-smash-lesbian-punch-wall-gif-4790446',
+'https://tenor.com/view/anime-naruto-punch-fight-gif-12911685',
+'https://tenor.com/view/anime-punch-knockout-wasted-smack-gif-11451829',
+'https://tenor.com/view/die-anime-punch-kill-angry-gif-8932977',
+'https://tenor.com/view/rin243109-blue-exorcist-anime-punch-gif-13785833',
+'https://tenor.com/view/shy-punch-anime-aki-adagaki-gif-12003970',
+'https://tenor.com/view/naruto-sasuke-punch-snicker-anime-gif-7380310',
+'https://tenor.com/view/stay-down-punch-naruto-gif-5211928',
+'https://tenor.com/view/fight-anime-punch-gif-16681456',
+'https://tenor.com/view/azumanga-daioh-azumanga-anime-punch-uppercut-gif-17075114',
 ];
 
 var rand = list[Math.floor(Math.random() * list.length)];
