1d177583eeca9aabae4bd20973028352c0445896
Glitch (crystal-panoramic-litter) <none>
2020-09-16 16:16:46 +0000

    🏐❣️ Checkpoint ./commands/fun/triggered.js:19971311/1549

--- commands/fun/triggered.js
+++ commands/fun/triggered.js
@@ -1,16 +1,26 @@
+const canvacord = require ("canvacord")
+
+const { MessageAttachment } = require("discord.js")
 module.exports= {
-  name:"",
-  description:""
-run = async (bot, message, args) => {
-
-    let profilepic = message.author.avatarURL;
-
-    message.channel.send({
-        file: {
-            attachment: "https://cute-api.tk/v1/generate/triggered?url=" + profilepic,
-            name: "triggered.gif"
-        }
-    });
-
+  name:"triggered",
+  description:"trigger some one",
+  category:"fun",
+  aliases:["tr"],
+run: async (client, message, args) => {
+async function create() {
+    let img = await canvacord.trigger("./image.png");
+    canvacord.write(img, "triggered.gif");
+ 
+    let color = await canvacord.color("#4E5D94");
+    canvacord.write(color, "color.png");
 }
+ 
+create()
+
+    let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
+    let triggered = await canvacord.trigger(user.displayAvatarURL({ format: "png", dynamic: false }));
+    let attachment = new MessageAttachment(triggered, "triggered.gif");
+    return message.channel.send(attachment);
+}
+
 }
