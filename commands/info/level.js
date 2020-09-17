c55bb27b1e7d1d869fc067fc3cdef38c1ba830ce
Glitch (crystal-panoramic-litter) <none>
2020-09-17 08:08:21 +0000

    📕🌎 Checkpoint ./commands/info/level.js:19971311/3583

--- commands/info/level.js
+++ commands/info/level.js
@@ -1,54 +1,37 @@
-const { MessageAttachment } = require("discord.js");
-
+const db = require('quick.db')
+const discord = require('discord.js')
+const { getInfo } = require("../../handlers/xp.js")
 module.exports = {
-  name:"Level",
-  description:"Show the level/rank",
-  category:"info",
-  aliases:["rank"],
-run: async (client, message, args) => {
-  let user =
-    message.mentions.users.first() ||
-    client.users.cache.get(args[0]) ||
-    match(args.join(" ").toLowerCase(), message.guild) ||
-    message.author;
-
-  let level = client.db.get(`level_${user.id}`) || 0;
-  level = level.toString();
-  let exp = client.db.get(`xp_${user.id}`) || 1;
-  let neededXP = Math.floor(Math.pow(level / 2,2));
-
-  let every = client.db
-    .all()
-    .filter(i => i.ID.startsWith("xp_"))
-    .sort((a, b) => b.data - a.data);
-  let rank = every.map(x => x.ID).indexOf(`xp_${user.id}`) + 1;
-  rank = rank.toString();
-  let img = await client.canvas.rank({
+  name: "level",
+  description: "Get the level of author or mentioned",
+  usage: "level <user>",
+  category: "info",
+  run: (client, message, args) => {
+    const user = message.mentions.users.first() || message.author;
+    
+    if(user.id === client.user.id) { //IF BOT
+      return message.channel.send("😉 | I am on level 100")
+    }
+    
+    if(user.bot) {
+      return message.channel.send("Bot do not have levels")
+    }
+    
+    let xp = db.get(`xp_${user.id}_${message.guild.id}`) || 0;
+    
+    const {level, remxp, levelxp} = getInfo(xp);
+    if(xp === 0) return message.channel.send(`**${user.tag}** is out of the xp`)
+  let Level = level.toString()
+   var img = await client.canvas.rank({
     username: user.username,
     discrim: user.discriminator,
     currentXP: exp.toString(),
     neededXP: neededXP.toString(),
-    rank,
     level,
     avatarURL: user.displayAvatarURL({ format: "png" }),
     background: "https://images.unsplash.com/photo-1523821741446-edb2b68bb7a0?ixlib=rb-1.2.1&w=1000&q=80"
   });
   return message.channel.send(new MessageAttachment(img, "rank.png"));
-
-function match(msg, i) {
-  if (!msg) return undefined;
-  if (!i) return undefined;
-  let user = i.members.cache.find(
-    m =>
-      m.user.username.toLowerCase().startsWith(msg) ||
-      m.user.username.toLowerCase() === msg ||
-      m.user.username.toLowerCase().includes(msg) ||
-      m.displayName.toLowerCase().startsWith(msg) ||
-      m.displayName.toLowerCase() === msg ||
-      m.displayName.toLowerCase().includes(msg)
-  );
-  if (!user) return undefined;
-  return user.user;
 }
-}
+  }
 }
