c8596853d2f51c8a6606387b8fdafcbdaaa9773d
Glitch (crystal-panoramic-litter) <none>
2020-09-17 11:55:22 +0000

    🐝🐋 Checkpoint ./commands/fun/triggered.js:19971311/89

--- commands/fun/triggered.js
+++ commands/fun/triggered.js
@@ -2,14 +2,14 @@ const canvacord = require ("canvacord")
 
 const { MessageAttachment } = require("discord.js")
 module.exports= {
-  name:"wasted",
-  description:"kill some one",
+  name:"triggered",
+  description:"trigger some one",
   category:"fun",
-  aliases:["ws"],
+  aliases:["tg"],
 run: async (client, message, args) => {
 async function create() {
-    let img = await canvacord.wasted("./image.png");
-    canvacord.write(img, "wasted.gif");
+    let img = await canvacord.triggered("./image.png");
+    canvacord.write(img, "triggered.gif");
  
     let color = await canvacord.color("#4E5D94");
     canvacord.write(color, "color.png");
@@ -18,7 +18,7 @@ async function create() {
 create()
 
     let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
-    let faceplam = await canvacord.wasted(user.displayAvatarURL({ format: "png", dynamic: false }));
+    let faceplam = await canvacord.triggered(user.displayAvatarURL({ format: "png", dynamic: false }));
     let attachment = new MessageAttachment(faceplam, "facepalm.gif");
     return message.channel.send(attachment);
 }
