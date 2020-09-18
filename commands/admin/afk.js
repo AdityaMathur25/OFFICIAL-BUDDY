afceb245b3ca6124dbe108f107cbb64de40d3864
Glitch (crystal-panoramic-litter) <none>
2020-09-18 12:12:11 +0000

    💫🕵 Checkpoint ./commands/admin/afk.js:19971311/716

---
+++ commands/admin/afk.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name:"setafk",
+  description:"SET YOUR AFK",
+  usage:"<@afk><@reason>",
+  aliases:["afk"],
+run = async (bot, message, args) => {
+
+    let reason = args.join(' ') ? args.join(' ') : 'I am currently afk, I will reply as soon possible.';
+    let afklist = bot.afk.get(message.author.id);
+
+    if (!afklist) {
+        let construct = {
+            id: message.author.id,
+            reason: reason
+        };
+
+        bot.afk.set(message.author.id, construct);
+        return message.reply(`you have been set to afk for reason: ${reason}`).then(msg => msg.delete(5000));
+    }
+
+};
+
+
+};
