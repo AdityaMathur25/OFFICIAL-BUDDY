9337383d3f523ea803848eaa179d256caf4c864f
Glitch (crystal-panoramic-litter) <none>
2020-09-18 05:26:33 +0000

    🌿💓 Checkpoint ./commands/info/level.js:19971311/282

--- commands/info/level.js
+++ commands/info/level.js
@@ -21,13 +21,13 @@ var user = message.mentions.users.first() || message.author;
     
     let xp = db.get(`xp_${user.id}_${message.guild.id}`) || 0;
 
-    const {level, remxp, levelxp} = getInfo(xp);
+    const {level, remxp, levelxp, every} = getInfo(xp);
     if(xp === 0) return message.channel.send(`**${user.tag}** is out of the xp`)
 const finallevel = level
 var loadingMsg = await message.channel.send('Loading image...');
-var rank = level;
-    rank = rank.toString();
-var finalRank = `#${rank}`;
+    var rank = every.map(x => x.ID).indexOf(`guild_${message.guild.id}_xptotal_${user.id}`) + 1;
+rank = rank.toString();
+    let finalRank = rank
     let image = await Canvacord.rank({
   username: user.username,
   discrim: user.discriminator,
@@ -37,7 +37,7 @@ var finalRank = `#${rank}`;
   level: finallevel,
    rank: finalRank,
   avatarURL: user.displayAvatarURL({ dynamic: true, format: "png" }),
-  color: 'Aqua',
+  color: 'white',
   background: await Canvacord.blur('https://i.pinimg.com/originals/76/0e/d7/760ed7f52c90870503762ac92db92adc.jpg')
 });
 
