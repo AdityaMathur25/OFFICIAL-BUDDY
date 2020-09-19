1e80a9765a8a173ed24f7e2c97e0b095c2512ad1
Glitch (crystal-panoramic-litter) <none>
2020-09-19 08:25:53 +0000

    Rewound to commit b0e87d93de418a579aa2a56c55745752309decc9 + 0 edits

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -13,33 +13,33 @@ module.exports = {
 
         //Start
 
-        //THIS IS THE CODE FOR THE COMMAND ONLY
- 
-    const messageArray = message.content.split(' ');
-	
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You Don't Have Permission To Use This Command!")
 
-    if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send('Lack of Perms!');
-    
-    let deleteAmount;
+        if (!args[0]) return message.channel.send(`Please Give Me Amounts Of Messages!`);
 
-    if (isNaN(args[0]) || parseInt(args[0]) <= 0) { return message.reply('Please put a number only!') }
+        if (isNaN(args[0])) return message.channel.send(`Please Give Me Number Value!`);
 
-    if (parseInt(args[0]) > 100) {
-        return message.reply('You can only delete 100 messages at a time!')
-    } else {
-        deleteAmount = parseInt(args[0]);
-    }
+        if (args[0] > 100) return message.channel.send(`I Can Only Delete 100 Messages Because Of Discord Limit!`);
 
-    message.channel.bulkDelete(deleteAmount + 1, true);
+        if (args[0] < 4) return message.channel.send(`You Can Delete ${args[0]} By Your Self Its Not Too Many Messages!`)
+
+        let Channel = message.channel;
+
+        let Reason = args.slice(1).join(" ");
+
+        message.channel.bulkDelete(args[0])
+        .then(Amount => {
             let embed = new Discord.MessageEmbed()
             .setColor("RANDOM")
             .setTitle(`Messages Deleted!`)
             .addField(`Moderator`, `${message.author.tag} (${message.author.id}`)
-        .addField(`Deleted Messages`, `${deleteAmount}`)
+            .addField(`Channel`, `${Channel.name} (${Channel.id}`)
+            .addField(`Deleted Messages`, `${Amount.size}`)
+            .addField(`Reason`, `${Reason || "No Reason Provided!"}`)
             .setFooter(`Requested by ${message.author.username}`)
             .setTimestamp();
-      message.reply(embed)
-        
+        message.channel.send(embed).then(msg => msg.delete({ timeout: 1000 }))
+        });
 
         //End
 
