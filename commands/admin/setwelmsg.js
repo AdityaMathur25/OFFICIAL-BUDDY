28631ec7e865298f85b73404e1326e58cec0a782
Glitch (crystal-panoramic-litter) <none>
2020-09-19 09:58:24 +0000

    🌻🐷 Checkpoint ./server.js:19971311/53 ./commands/moderation/welcome.js:19971311/1350 ./commands/admin/setwelmsg.js:19971311/913

---
+++ commands/admin/setwelmsg.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js")
+const db = require("quick.db")
+
+module.exports = {
+  name: "setwelmessage",
+  category: "Administration",
+  usage: "setwelcomemessage <@message>",
+  description: "Set the welcome message",
+  alaises:["msg"],
+  run: (client, message, args) => {
+    
+    let channel = message.content.first() //mentioned channel
+    
+    if(!channel) { //if channel is not mentioned
+      return message.channel.send("Please provide message")
+    }
+    
+    //Now we gonna use quick.db
+    
+    db.set(`welchannel_${message.guild.id}`, message.content) //set id in var
+    
+    message.channel.send(`Welcome Channel is seted as ${channel}`) //send success message
+  }
+}
--- server.js
+++ server.js
@@ -90,6 +90,7 @@ client.on("guildMemberAdd", async member => {
   let random = Math.floor(Math.random() * 5); //no i dont want 4 image 1 omly
   let data = await canva.welcome(member, { link: `${images[random]}` });
   const attachment = new discord.MessageAttachment(data, "welcome-image.png");
+  let msg = db.get(`msg_${member.guild.id}`)
   client.channels.cache
     .get(chx)
     .send(`Welcome to the server, ${member.user.username}!`, attachment);
