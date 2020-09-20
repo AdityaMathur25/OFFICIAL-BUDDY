652be56fb31e13be4a818fea9b0fb43f65ad8eb7
Glitch (crystal-panoramic-litter) <none>
2020-09-20 16:23:19 +0000

    🐑🎭 Checkpoint ./server.js:19971311/2560

--- server.js
+++ server.js
@@ -21,9 +21,8 @@ client.commands = new Collection();
 client.aliases = new Collection();
 client.categories = fs.readdirSync("./commands");
 
- if(!message.member.hasPermission("ADMINISTRATOR")) {
-   
- }
+ 
+    
 
 // Run the command loader
 ["command"].forEach(handler => {
@@ -227,7 +226,46 @@ client.on("guildDelete", guild => {
       } catch (error) {
         console.log(`Error :\n${error}`);
       }
+    
+    
     }}})
+function is_url(str) {
+  let regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  if(regexp.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
+  
+}
+client.on("message", async message => {
+  if (message.author.bot) return;  
+  //START
+  if(!message.member.hasPermission("ADMINISTRATOR")) {
+    
+    if(is_url(message.content) === true) {
+      message.delete()
+      return message.channel.send("You can not send link here :/")
+    }
+    let confirm = false;
+    //NOW WE WILL USE FOR LOOP
+    var i;
+    for(i = 0;i < badwords.length; i++) {
+      
+      if(message.content.toLowerCase().includes(badwords[i].toLowerCase()))
+        confirm = true;
+      
+    }
+    
+    if(confirm) {
+      message.delete()
+      const hp = new discord.Messageembed()
+      return message.channel.send("You are not allowed to send badwords here")
+    }    
+    
+    
+  }})
+
     
   
 
