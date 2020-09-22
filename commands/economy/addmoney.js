1a31e9b6a239736eafbd1777698e429c60dfd683
Glitch (crystal-panoramic-litter) <none>
2020-09-22 18:17:06 +0000

    🏛🍳 Checkpoint ./commands/economy/addmoney.js:19971311/1323

---
+++ commands/economy/addmoney.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+
+module.exports = {
+  name: "addmoney",
+  category: "economy",
+  description: "add money to any user only admin",
+run: async (bot, message, args) => {
+
+
+
+  
