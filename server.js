f52e154717fd8ab28ed2aafb5ad62a2ababdf7f9
Glitch (crystal-panoramic-litter) <none>
2020-09-22 06:27:37 +0000

    🍂🗼 Checkpoint ./server.js:19971311/1608

--- server.js
+++ server.js
@@ -273,16 +273,23 @@ client.on("message", async message => {
   }})
  // This runs the filter on any message b
 antispam(client, {
-        limitUntilWarn: 3, // The amount of messages allowed to send within the interval(time) before getting a warn.
-        limitUntilMuted: 5, // The amount of messages allowed to send within the interval(time) before getting a muted.
-        interval: 2000, // The interval(time) where the messages are sent. Practically if member X sent 5+ messages within 2 seconds, he get muted. (1000 milliseconds = 1 second, 2000 milliseconds = 2 seconds etc etc)
-        warningMessage: "Don't spam here", // Message you get when you are warned!
-        muteMessage: "was muted",// Message sent after member X was punished(muted).
-       kickMessage: "was kicked", //Message sent after member X was kicked from guild!
-       banMessage: "was banned", //Message sent after member X was banned from the guild!
-        maxDuplicatesWarning: 7,// When people are spamming the same message, this will trigger when member X sent over 7+ messages.
-        maxDuplicatesMute: 10, // The limit where member X get muted after sending too many messages(10+).
-        ig
+        limitUntilWarn: 3,
+        limitUntilMuted: 5,
+        interval: 2000,
+        warningMessage: "STOP SPAMING OR I WILL PUNISH YOU !",
+        muteMessage: "HAS BEEN MUTED",
+        banMessage: "HAS BEEN BANNED",
+        kickMessage: "HAS BEEN KICKED",
+        maxDuplicatesWarning: 7,
+        maxDuplicatesMute: 10,
+        ignoredRoles: ["SERVER BOSS"],
+        ignoredMembers: [],
+        ignoreBots: true,
+        ignoredChannels: ["chat"],
+       ignorePermissions: ["ADMINISTRATOR"],
+        mutedRole:"Muted",
+        timeMuted: 1000*600,
+        logChannel: "#log",
 })
 client.on('message', msg => {
   client.emit('checkMessage', msg); // This runs the filter on any message bot receives in any guilds.
