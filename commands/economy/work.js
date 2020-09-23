3574d7fa22be580d343bf47a55c8d359312d34e3
Glitch (crystal-panoramic-litter) <none>
2020-09-23 05:20:24 +0000

    🐊🎆 Checkpoint ./commands/economy/work.js:19971311/160 ./commands/economy/weekly.js:19971311/1257

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -1,19 +1,17 @@
 const db = require('quick.db');
 const ms = require('parse-ms');
-const Discord = requir
+const Discord = require('discord.js')
 module.exports = {
     name: "work",
     description: "Work for your money",
     category: "economy",
     async run (client, message, args) {
-        const db = require('quick.db')
-
     
     if (args[0] == 'prostitute') {
 
         let amount = Math.floor(Math.random() * 500) + 1; // 1-500 random number. whatever you'd like
 
-        let embed = new Discord.RichEmbed()
+        let embed = new Discord.MessageEmbed()
         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL) 
         .setDescription(`${message.author}, you worked as a prostitute & got payed ${amount}$ for having sex! :D`)
         .setColor("RANDOM")
@@ -24,7 +22,7 @@ module.exports = {
     } else if(args[0] == 'constructor') {
         let amount = Math.floor(Math.random() * 500) + 1; // 1-500 random number. whatever you'd like
 
-        let embed = new Discord.RichEmbed()
+        let embed = new Discord.MessageEmbed()
         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL) 
         .setDescription(`${message.author}, you worked as a constructor & got payed ${amount}$ for rebuilding the empire state building.`)
         .setColor("RANDOM")
@@ -35,14 +33,16 @@ module.exports = {
     } else if(args[0] == 'programmer') {
         let amount = Math.floor(Math.random() * 500) + 1; // 1-500 random number. change to whatever you'd like
 
-        let embed = new Discord.RichEmbed()
+        let embed = new Discord.MessageEmbed()
         .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL) 
         .setDescription(`${message.author}, you worked as a programmer for epicgames, you fixed their game & earned ${amount}$!`)
         .setColor("RANDOM")
         
     
         message.channel.send(embed)
-        db.add(`money_${message.author.id}`, amount)
+        db.add(`money_${message.guild.id}_${message.author.id}`, amount)
     }
 
+    }
+}
 
