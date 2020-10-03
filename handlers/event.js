97d70e62552ab6283ae7c4b7a54711d920da2b13
Glitch (crystal-panoramic-litter) <none>
2020-10-03 08:06:23 +0000

    🗼🍭 Checkpoint ./handlers/event.js:19971311/839

--- handlers/event.js
+++ handlers/event.js
@@ -11,22 +11,20 @@ module.exports = (client) => {
         // Filter so we only have .js command files
         const commands = readdirSync(`./events/`).filter(file => file.endsWith(".js"));
     
-        // Loop over the commands, and add all of them to a collection
-        // If there's no name found, prevent it from returning an error,
-        // By using a cross in the table we made.
-        for (let file of commands) {
-            let pull = require(`../commands/${dir}/${file}`);
-    
-            if (pull.name) {
-                client.commands.set(pull.name, pull);
-                table.addRow(file, '✅');
-            } else {
+  for (let files of commands ){
+         try { 
+         let pull = require(`../events/${file}`);
+    if (pull.event && typeof pull.event !== "string")
+                table.addRow(file, '❌ plse join ctk server :)');
+              continue;
+            } 
                 table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
                 continue;
             }
     
+    
             // If there's an aliases key, read the aliases.
-            if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
+            
         }
     });
     // Log the table
