06a5b7c74b46e1dd088ea56f77b7536eae845ea6
Glitch (crystal-panoramic-litter) <none>
2020-10-03 06:22:55 +0000

    👸🕰 Checkpoint ./config.json:19971311/2 ./commands/owner/restart.js:19971311/19 ./commands/owner/eval.js:19971311/197

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -4,16 +4,14 @@ module.exports = {
   name: "eval",
   description:"check command",
   category:"owner",
+  ownerOnly: true,
   run: async (client, message, args) => {
  
 let process = args.join(" ");
 if(!process) {
 return message.channel.send("Please give a code to evaluate!");
   }
-let ids = ['480285300484997122', '539385330923601930']
-if(!ids.includes(message.author.id)) {
-  return message.channel.send("You do not have permissions to use this command")
-  }
+
 let e;
  try {
 e = eval(process)
