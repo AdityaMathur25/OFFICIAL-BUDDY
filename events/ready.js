a5c7aa4fbdd570333cf8843c9caa3d1ea18ccbe8
Glitch (crystal-panoramic-litter) <none>
2020-10-03 12:47:51 +0000

    🍨📊 Checkpoint ./events/ready.js:19971311/271 ./handlers/event.js:19971311/2078

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,6 @@
+const db = require("quick.db")
+
 module.exports.run = (client) => {
-  console.log("testing")
+  console.log("JOIN CTK SERVER : https://withwin.in/dbd" )
+  client.user.setActivity(db.get(`status`)); 
 }
--- handlers/event.js
+++ handlers/event.js
@@ -1,32 +1,39 @@
 const { readdirSync } = require("fs");
+
 const ascii = require("ascii-table");
+
 
 // Create a new Ascii table
 let table = new ascii("Events");
-table.setHeading("events", "Load status");
+table.setHeading("Events", "Load status");
 
 module.exports = (client) => {
-    // Read every commands subfolder
-    readdirSync("./events/").forEach(dir => {
-        // Filter so we only have .js command files
-        const commands = readdirSync(`./events/`).filter(file => file.endsWith(".js"));
-    
-        for(let files of commands ) {
-       
-          try { 
-         let pull = require(`../events/${file}`);
-    if (pull.event && typeof pull.event !== "string")
-                table.addRow(file, '❌ plse join ctk server :)');
-              continue;
-          }
-          pull.event = pull.event|| file.replace(".js", "")
-          client.on(pull.event, pull.run.bind(null, client))
-                table.addRow(file, `✔️`);
-                continue;
-          }
-      catch(err){
-              console.log("something went wrong")
-              table.addRow(`✖️ CALL CTK OR DIE`)
-      }})
-                    console.log(table.tostring())
-                                    }
+
+  const commands = readdirSync(`./events/`).filter(file => file.endsWith(".js"));
+
+  for (let file of commands) {
+
+    try {
+    let pull = require(`../events/${file}`);
+
+    if (pull.event && typeof pull.event !== "string") {
+      table.addRow(file, `❌ -> Property event should be string.`);
+      continue;
+    }
+
+    pull.event = pull.event || file.replace(".js", "")
+
+    client.on(pull.event, pull.run.bind(null, client))
+
+    table.addRow(file, '✅');
+
+    } catch(err) {
+
+  console.log("Error While loading/executing command, join for help : https://withwin.in/dbd")
+  console.log(err)
+  table.addRow(file, `❌ -> Error while loading event, join for help :  https://withwin.in/dbd`);
+    }
+  }
+
+   console.log(table.toString());
+}
