f41028aae7d32c6a78a1e0c745c57e698e40a0ac
Glitch (crystal-panoramic-litter) <none>
2020-10-03 18:13:25 +0000

    🌂🌗 Checkpoint ./commands/music/play.js:19971311/27312 ./events/message.js:13134877/8276:19971311/19401

--- events/message.js
+++ events/message.js
@@ -1,31 +1,27 @@
-const db = require("quick.db")
+const db = require("quick.db");
 const { addexp } = require("../handlers/xp.js");
 const { ownerID, default_prefix } = require("../config.json");
-const { badwords } = require("../data.json") 
-let cooldown = {}
+const { badwords } = require("../data.json");
+let cooldown = {};
 
-module.exports.run = async (client, message, guild) => {
+module.exports.run = async (client, message) => {
   if (message.author.bot) return;
   if (!message.guild) return;
 
   addexp(message);
 
   if (!message.member.hasPermission("ADMINISTRATOR")) {
-
-
-
     message.content.split(" ").forEach(m => {
       if (is_url(m)) {
-        message.delete().catch(err => {})
-        return message.channel.send("You are not allowed to send links :/")
+        message.delete().catch(err => {});
+        return message.channel.send("You are not allowed to send links :/");
       } else if (badwords.find(x => x.toLowerCase() === m.toLowerCase())) {
-
-        message.delete().catch(err => {})
-        return message.channel.send("You are not allowed to use (**" + m + "**) word here")
-
+        message.delete().catch(err => {});
+        return message.channel.send(
+          "You are not allowed to use (**" + m + "**) word here"
+        );
       }
-    })
-
+    });
   }
 
   let prefix = db.get(`prefix_${message.guild.id}`);
@@ -44,11 +40,11 @@ module.exports.run = async (client, message, guild) => {
 
   if (cmd.length === 0) return;
 
-  let cmdx = db.get(`cmd_${message.guild.id}`)
+  let cmdx = db.get(`cmd_${message.guild.id}`);
 
   if (cmdx) {
-    let cmdy = cmdx.find(x => x.name === cmd)
-    if (cmdy) message.channel.send(cmdy.responce)
+    let cmdy = cmdx.find(x => x.name === cmd);
+    if (cmdy) message.channel.send(cmdy.responce);
   }
 
   // Get the command
@@ -56,36 +52,41 @@ module.exports.run = async (client, message, guild) => {
   // If none is found, try to find it by alias
   if (!command) command = client.commands.get(client.aliases.get(cmd));
 
-
   if (!command) return;
 
   //-------------------------------------------- P E R M I S S I O N -------------------------------------------
 
-
-
   if (command.botPermission) {
-    let neededPerms = []
+    let neededPerms = [];
 
     command.botPermission.forEach(p => {
-      if (!message.guild.me.hasPermission(p)) neededPerms.push("`" + p + "`")
-    })
+      if (!message.guild.me.hasPermission(p)) neededPerms.push("`" + p + "`");
+    });
 
-    if (neededPerms.length) return message.channel.send(`I need ${neededPerms.join(", ")} permission(s) to execute the command!`)
+    if (neededPerms.length)
+      return message.channel.send(
+        `I need ${neededPerms.join(", ")} permission(s) to execute the command!`
+      );
   } else if (command.authorPermission) {
-    let neededPerms = []
-
+    let neededPerms = [];
 
     command.authorPermission.forEach(p => {
-      if (!message.member.hasPermission(p)) neededPerms.push("`" + p + "`")
-    })
+      if (!message.member.hasPermission(p)) neededPerms.push("`" + p + "`");
+    });
 
-    if (neededPerms.length) return message.channel.send(`You need ${neededPerms.join(", ")} permission(s) to execute the command!`)
+    if (neededPerms.length)
+      return message.channel.send(
+        `You need ${neededPerms.join(
+          ", "
+        )} permission(s) to execute the command!`
+      );
   }
 
   // ---------------------------------------------O W N E R ----------------------------------------------------------
 
   if (command.ownerOnly) {
-    if (message.author.id !== ownerID) return message.channel.send("This command can only be use by owner :C")
+    if (message.author.id !== ownerID)
+      return message.channel.send("This command can only be use by owner :C");
   }
 
   //------------------------------------------------------COOLDOWN SYSTEM---------------------------------------------
@@ -93,13 +94,18 @@ module.exports.run = async (client, message, guild) => {
   let uCooldown = cooldown[message.author.id];
 
   if (!uCooldown) {
-    cooldown[message.author.id] = {}
-    uCooldown = cooldown[message.author.id]
+    cooldown[message.author.id] = {};
+    uCooldown = cooldown[message.author.id];
   }
 
-  let time = uCooldown[command.name] || 0
+  let time = uCooldown[command.name] || 0;
 
-  if (time && (time > Date.now())) return message.channel.send(`You can again use this command in ${Math.ceil((time - Date.now()) / 1000)} second(s)`) //YOU CAN USE PARSE MS TO GET BETTER responce
+  if (time && time > Date.now())
+    return message.channel.send(
+      `You can again use this command in ${Math.ceil(
+        (time - Date.now()) / 1000
+      )} second(s)`
+    ); //YOU CAN USE PARSE MS TO GET BETTER responce
 
   cooldown[message.author.id][command.name] = Date.now() + command.cooldown;
 
@@ -108,23 +114,14 @@ module.exports.run = async (client, message, guild) => {
   //-----------------------------------------------------------------------------------------------------------------
 
   if (command) command.run(client, message, args);
- 
-
-
-
-
-
-
-}
-
+};
 
 //-------------------------------------------- F U N C T I O N ------------------------------------------
 function is_url(str) {
   let regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-  if(regexp.test(str)) {
+  if (regexp.test(str)) {
     return true;
   } else {
     return false;
   }
-  
 }
