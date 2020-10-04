832ed49f4f875e05297fc19204ea1b6bd5d71aa4
Glitch (crystal-panoramic-litter) <none>
2020-10-04 06:58:46 +0000

    😼🚜 Checkpoint ./events/ready.js:19971311/67

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,6 @@ const db = require("quick.db")
 module.exports.run = (client) => {
   console.log(client.user.tag + "ready")
   let xx = `${db.get(`status`)}`
-  client.user.setActivity(db.get(`status`), { type: "WATCHING" });
   client.user.setPresence({
 status: "idle", 
 activity: { 
