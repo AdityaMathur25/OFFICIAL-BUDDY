222c4137351fcd34dd1ebeb75805a4d5768ba196
Glitch (crystal-panoramic-litter) <none>
2020-10-07 12:27:39 +0000

    🏛🍧 Checkpoint ./commands/fun/tweet.js:19971311/183

--- commands/fun/tweet.js
+++ commands/fun/tweet.js
@@ -3,16 +3,17 @@ module.exports = {
   name: "tweet",
   category: "fun",
  description: "Make joke of ur Friends 😂",
-  usage: "",
-  aliases: [""],
+  usage: "<@mention> <message>",
+  aliases: ["tw"],
   run: async (client, message, args) => {
-   let user = message.mentions.members.first() || message.author ;
-    if (!user) return message.channel.send("MENTION USER PLEASE")
-   let mm = message.content 
-   if (mm === null) return
-    message.channel.send("Provide tweet message ")
+   let user = message.mentions.users.first() || message.author ;
+    if (!user)
+      return message.channel.send("MENTION USER PLEASE")
+   let mm = message.content
+   if (!mm)
+   return message.channel.send("Provide tweet message ")
      
-   let TweetEmbed = await Random.Tweet( user, mm, "RANDOM");
+   let TweetEmbed = await Random.Tweet( user.name, mm, "RANDOM");
 message.channel.send(TweetEmbed)
     
     
