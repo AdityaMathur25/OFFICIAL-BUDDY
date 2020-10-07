b655392c4cb1f31367b0b24891001a4a87ec99cc
Glitch (crystal-panoramic-litter) <none>
2020-10-07 14:16:28 +0000

    Rewound to commit f63df009401b4eff712a63c476f2bc7c6aacc9f8 + 0 edits

--- system/music.js
+++ system/music.js
@@ -1,64 +1,63 @@
-
-
-
-
-
-
-  
-          let lastsong = queue.songs.shift();
-          module.exports.play(queue.songs[0], message);
-          module.exports.play(queue.songs[0], message);
-          queue.songs.push(lastsong);
-          queue.songs.shift();
-        .catch(console.error);
-        .send(embed)
-        console.error(error);
-        highWaterMark: 1 << 25
-        if (queue.loop) {
-        module.exports.play(queue.songs[0], message);
-        queue.songs.shift();
-        return message.channel.send("THIS VIDEO CONTAINS COPYRIGHT CONTENT");
-        }
-        } else {
-      .catch(err => message.channel.send("UNABLE TO PLAY SONG"));
-      .on("error", console.error);
-      .on("finish", () => {
-      .play(stream, { type: "opus" })
-      .send(embed)
-      embed.setAuthor("MUSIC QUEUE IS ENDED NOW :/")
-      if (error.message.includes === "copyright") {
-      if (queue) {
-      message.client.queue.delete(message.guild.id);
-      queue.channel.leave();
-      return queue.textChannel
-      var stream = await ytdlDiscord(song.url, {
-      }
-      }
-      } else {
-      })
-      });
-    .setDescription(`**[${song.title}](${song.url})**`)
-    .setFooter(`REQUESTED BY ${message.author.username}`)
-    .setThumbnail(song.thumbnail)
-    const dispatcher = queue.connection
-    const queue = message.client.queue.get(message.guild.id);
-    dispatcher.setVolumeLogarithmic(queue.volume / 100); //VOLUME
-    if (!song) {
-    let put = nsk
-    queue.textChannel
-    try {
-    }
-    }
-    } catch (error) {
-  async play(song, message) {
-  }
-.setColor(COLOR);
 //I WILL BE BACK AFTER 5 min
-const prism = require("prism-media");
 const ytdlDiscord = require("ytdl-core-discord");
 const { MessageEmbed } = require("discord.js")
 const { QUEUE_LIMIT, COLOR } = require("../config.json");
-embed.setAuthor("Started Playing Song", message.author.displayAvatarURL({dynamic: true}))
-let embed = new MessageEmbed()
+const prism = require("prism-media");
+
 module.exports = {
+  async play(song, message) {
+    const queue = message.client.queue.get(message.guild.id);
+let embed = new MessageEmbed()
+.setColor(COLOR);
+
+    if (!song) {
+      queue.channel.leave();
+      message.client.queue.delete(message.guild.id);
+      embed.setAuthor("MUSIC QUEUE IS ENDED NOW :/")
+      return queue.textChannel
+        .send(embed)
+        .catch(console.error);
+    }
+
+    try {
+      var stream = await ytdlDiscord(song.url, {
+        highWaterMark: 1 << 25
+      });
+    } catch (error) {
+      if (queue) {
+        queue.songs.shift();
+        module.exports.play(queue.songs[0], message);
+      }
+
+      if (error.message.includes === "copyright") {
+        return message.channel.send("THIS VIDEO CONTAINS COPYRIGHT CONTENT");
+      } else {
+        console.error(error);
+      }
+    }
+    let put = nsk
+
+    const dispatcher = queue.connection
+      .play(stream, { type: "opus" })
+      .on("finish", () => {
+        if (queue.loop) {
+          let lastsong = queue.songs.shift();
+          queue.songs.push(lastsong);
+          module.exports.play(queue.songs[0], message);
+        } else {
+          queue.songs.shift();
+          module.exports.play(queue.songs[0], message);
+        }
+      })
+      .on("error", console.error);
+  
+    dispatcher.setVolumeLogarithmic(queue.volume / 100); //VOLUME
+embed.setAuthor("Started Playing Song", message.author.displayAvatarURL({dynamic: true}))
+    .setDescription(`**[${song.title}](${song.url})**`)
+    .setThumbnail(song.thumbnail)
+    .setFooter(`REQUESTED BY ${message.author.username}`)
+    queue.textChannel
+      .send(embed)
+      .catch(err => message.channel.send("UNABLE TO PLAY SONG"));
+  }
 };
