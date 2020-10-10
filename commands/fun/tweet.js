f3a9777d37c3425b3106d72e69dfd1cc0dd3d485
Glitch (crystal-panoramic-litter) <none>
2020-10-10 09:42:36 +0000

    🚲🚅 Checkpoint ./commands/fun/tweet.js:13134877/1270:20536785/100

--- commands/fun/tweet.js
+++ commands/fun/tweet.js
@@ -1,20 +1,17 @@
-const Random = require("srod-v2")
+const Random = require("srod-v2");
 module.exports = {
   name: "tweet",
   category: "fun",
- description: "Make joke of ur Friends 😂",
+  description: "Make joke of ur Friends 😂",
   usage: "<@mention> <message>",
   aliases: ["tw"],
   run: async (client, message, args) => {
-   let user = message.mentions.members.first() || message.author ;
-    if (!user)
-      return message.channel.send("MENTION USER PLEASE")
-   let mm = args.slice(1).join(" ")
-   if (mm === null)
-   return message.channel.send("Provide tweet message ")
-     
-   let TweetEmbed = await Random.Tweet( user.username, mm, "RANDOM");
-message.channel.send(TweetEmbed)
-    
-    
-    }}
+    let user = message.mentions.members.first() || message.author;
+    if (!user) return message.channel.send("MENTION USER PLEASE");
+    let mm = args.slice(1).join(" ");
+    if (mm === null) return message.channel.send("Provide tweet message ");
+
+    let TweetEmbed =  await Random.Tweet(args[0], mm, "BLUE");
+    console.log(TweetEmbed)
+    message.channel.send(TweetEmbed);//else work or not idk lets try
+  }}
