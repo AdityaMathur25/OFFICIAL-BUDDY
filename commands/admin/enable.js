8d02d3b0dd8012655b155db6c0462c257844fbc2
Glitch (crystal-panoramic-litter) <none>
2020-10-11 16:10:55 +0000

    🎭🌎 Checkpoint ./package.json:20536785/2987 ./commands/admin/enable.js:20536785/284

--- commands/admin/enable.js
+++ commands/admin/enable.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 
-const { database }  = require("quickmongo")
-const db = new database("mongodb+srv://Buddy:123@cluster0.qqght.gcp.mongodb.net/test", "cluster0");
+const mongoose = require('mongoose');
+const db = mongoose.connect("mongodb+srv://Buddy:123@cluster0.qqght.gcp.mongodb.net/test");
 
 module.exports = {
 
