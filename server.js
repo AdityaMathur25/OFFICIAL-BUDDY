5ee1e1100f79674b11252a4e052ca07141aa2c23
Glitch (crystal-panoramic-litter) <none>
2020-10-15 15:09:28 +0000

    Rewound to commit 7226fda6a916e9754d2bc7f8f378c7fa46f4b2d5 + 0 edits

--- server.js
+++ server.js
@@ -1,161 +1,160 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
- 
-     
-        .setColor("GREEN")
-        .setColor("GREEN")
-        .setDescription(` YOU ARE NOT ALLOWED TO SEND BAD WORDS HERE!`)
-        .setDescription(` YOU ARE NOT ALLOWED TO SEND link HERE!`)
-        .setFooter("STOP USING BAD WORDS")
-        .setFooter("STOP USING LINKS")
-        .setTimestamp();
-        .setTimestamp();
-        .setTitle("**ANTI-BADWORD**")
-        .setTitle("**ANTI-LINK**")
-        confirm = true;
-        confirm = true;
-      .setAuthor(message.author.username, message.author.displayAvatarURL())
-      .setColor("RANDOM")
-      .setDescription(`HEY, MY PREFIX IN THIS SERVER IS **${prefix}**`)
-      .setFooter(`REQUESTED BY ${message.author.username}`);
-      .setTitle("PREFIX HELP! ")
-      if (message.content.toLowerCase().includes(badwords[i].toLowerCase()))
-      if (message.content.toLowerCase().includes(oks[i].toLowerCase()))
-      let gp = new MessageEmbed()
-      let gp = new MessageEmbed()
-      message.delete();
-      message.delete();
-      name: sta,
-      return message.channel.send(gp);
-      return message.channel.send(gp);
-      type: "PLAYING"
-    .addField("Server Members :", guild.memberCount)
-    .addField("Server Members :", guild.memberCount)
-    .addField("Server Name :", guild.name)
-    .addField("Server Name :", guild.name)
-    .addField("Server Owner :", guild.owner)
-    .addField("Server Owner :", guild.owner)
-    .addField("VERIFICATION LEVEL :", guild.verificationLevel);
-    .addField("VERIFICATION LEVEL :", guild.verificationLevel);
-    .setColor("#00FFFF")
-    .setColor("RED")
-    .setThumbnail(guild.iconURL())
-    .setTitle("LEFT FROM SERVER")
-    .setTitle("New Server Joined")
-    //NOW WE WILL USE FOR LOOP
-    //NOW WE WILL USE FOR LOOP
-    activity: {
-    client.user.setPresence({
-    const luck = new MessageEmbed()
-    for (i = 0; i < badwords.length; i++) {
-    for (i = 0; i < oks.length; i++) {
-    if (confirm) {
-    if (confirm) {
-    let confirm = false;
-    let confirm = false;
-    return message.channel.send(luck);
-    status: "idle",
-    var i;
-    var i;
-    }
-    }
-    }
-    }
-    }
-  //START
-  //START
-  //some error here
+const {
   Client,
   Collection,
   MessageAttachment,
   MessageEmbed
-  client.channels.cache.get("748936869022007376").send(join);
-  client.channels.cache.get("748936869022007376").send(join1);
-  console.log("LEFT FROM SERVER" + guild.name);
-  console.log("NEW SERVER JOIN" + guild.name);
-  disableEveryone: true
-  if (!message.member.hasPermission("ADMINISTRATOR")) {
-  if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-  if (message.author.bot) return;
-  if (message.author.bot) return;
-  if (message.mentions.has("@everyone")) return;
-  if (message.mentions.has(client.user)) {
-  if (prefix === null) prefix = default_prefix;
-  let join = new MessageEmbed()
-  let join1 = new discord.MessageEmbed()
-  let prefix = await db2.get(`prefix_${message.guild.id}`);
-  let sta =  await db.fetch(`status`)
-  require(`./handlers/${handler}`)(client);
-  }
-  }
-  });
-  }})
-// Collections
-// Run the command loader
-// for not taging everyone.
-//Stupid kid!
-//define message lol
-//for image ?
-//ok im stupid u do it thank you ! mam
-["command", "events"].forEach(handler => {
-client.aliases = new Collection();
-client.canvas = require("canvacord");
-client.categories = fs.readdirSync("./commands");
-client.commands = new Collection();
-client.login(process.env.ass);
-client.on("guildCreate", guild => {
-client.on("guildDelete", guild => {
-client.on("message", async message => {
-client.on("message", async message => {
-client.on("message", async message => {
-client.on("ready", async () => {
-client.queue = new Map();
-console.log("ready as badass");
-const canva = new CanvasSenpai();
-const client = new Client({
-const db = require("wio.db")
-const db2 = require('quick.db')
-const fs = require("fs");
-const project = require("project-uptimer");
-const {
-const { CanvasSenpai } = require("canvas-senpai");
-const { addexp } = require("./handlers/xp.js");
-const { badwords } = require("./data.json");
+} = require("discord.js");
 const { config } = require("dotenv");
 const { default_prefix, token, COLOR, ownerid } = require("./config.json");
+const fs = require("fs");
 const { discord, message } = require("discord.js");
-const { oks } = require("./link.json");
-let cooldown = {};
+const { CanvasSenpai } = require("canvas-senpai");
+const canva = new CanvasSenpai();
+const { addexp } = require("./handlers/xp.js");
+const db = require("wio.db")
+const db2 = require('quick.db')
+
+const project = require("project-uptimer");
+const { badwords } = require("./data.json");
 let random = Math.floor(Math.random() * 4);
+let cooldown = {};
+//for image ?
+const client = new Client({
+  disableEveryone: true
+});
+// for not taging everyone.
+// Collections
+client.canvas = require("canvacord");
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands");
+client.queue = new Map();
+
+// Run the command loader
+["command", "events"].forEach(handler => {
+  //some error here
+  require(`./handlers/${handler}`)(client);
+});
+ 
+console.log("ready as badass");
+
+client.on("ready", async () => {
+  let sta =  await db.fetch(`status`)
+ 
+    client.user.setPresence({
+    status: "idle",
+    activity: {
+      name: sta,
+      type: "PLAYING"
+    }
+  });
+});
+//Stupid kid!
+//define message lol
+//ok im stupid u do it thank you ! mam
+client.on("guildCreate", guild => {
+  let join = new MessageEmbed()
+    .setColor("#00FFFF")
+    .setTitle("New Server Joined")
+    .addField("Server Members :", guild.memberCount)
+    .addField("Server Name :", guild.name)
+    .setThumbnail(guild.iconURL())
+    .addField("Server Owner :", guild.owner)
+    .addField("VERIFICATION LEVEL :", guild.verificationLevel);
+  client.channels.cache.get("748936869022007376").send(join);
+  console.log("NEW SERVER JOIN" + guild.name);
+});
+client.on("guildDelete", guild => {
+  let join1 = new discord.MessageEmbed()
+    .setColor("RED")
+    .setTitle("LEFT FROM SERVER")
+    .addField("Server Members :", guild.memberCount)
+    .addField("Server Name :", guild.name)
+    .addField("Server Owner :", guild.owner)
+    .addField("VERIFICATION LEVEL :", guild.verificationLevel);
+  client.channels.cache.get("748936869022007376").send(join1);
+  console.log("LEFT FROM SERVER" + guild.name);
+});
+
+client.on("message", async message => {
+  if (message.author.bot) return;
+  //START
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+    let confirm = false;
+    //NOW WE WILL USE FOR LOOP
+    var i;
+    for (i = 0; i < badwords.length; i++) {
+      if (message.content.toLowerCase().includes(badwords[i].toLowerCase()))
+        confirm = true;
+    }
+
+    if (confirm) {
+      message.delete();
+      let gp = new MessageEmbed()
+        .setTitle("**ANTI-BADWORD**")
+        .setDescription(` YOU ARE NOT ALLOWED TO SEND BAD WORDS HERE!`)
+        .setFooter("STOP USING BAD WORDS")
+        .setColor("GREEN")
+        .setTimestamp();
+      return message.channel.send(gp);
+    }
+  }
+});
+client.on("message", async message => {
+
+  let prefix = await db2.get(`prefix_${message.guild.id}`);
+
+  if (prefix === null) prefix = default_prefix;
+
+  if (message.mentions.has("@everyone")) return;
+
+  if (message.mentions.has(client.user)) {
+
+    const luck = new MessageEmbed()
+
+      .setAuthor(message.author.username, message.author.displayAvatarURL())
+
+      .setTitle("PREFIX HELP! ")
+
+      .setDescription(`HEY, MY PREFIX IN THIS SERVER IS **${prefix}**`)
+
+      .setColor("RANDOM")
+
+      .setFooter(`REQUESTED BY ${message.author.username}`);
+
+    return message.channel.send(luck);
+
+  }})
+
+const { oks } = require("./link.json");
+client.on("message", async message => {
+  if (message.author.bot) return;
+  //START
+  if (!message.member.hasPermission("ADMINISTRATOR")) {
+    let confirm = false;
+    //NOW WE WILL USE FOR LOOP
+    var i;
+    for (i = 0; i < oks.length; i++) {
+      if (message.content.toLowerCase().includes(oks[i].toLowerCase()))
+        confirm = true;
+    }
+
+    if (confirm) {
+      message.delete();
+     
+      let gp = new MessageEmbed()
+        .setTitle("**ANTI-LINK**")
+        .setDescription(` YOU ARE NOT ALLOWED TO SEND link HERE!`)
+        .setFooter("STOP USING LINKS")
+        .setColor("GREEN")
+        .setTimestamp();
+      return message.channel.send(gp);
+    }
+  }
+});
+
+
 project.url("ajbdwk", 20000) //default is 1m (not required)
-} = require("discord.js");
-});
-});
-});
-});
-});
-});
-});
+
+
+client.login(process.env.ass);
