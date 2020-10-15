bd12e06d8cd9c63c33073d2f87806ef094e90478
Glitch (crystal-panoramic-litter) <none>
2020-10-15 15:44:58 +0000

    👠🎆 Checkpoint ./commands/utility/montior.js:20536785/509

--- commands/utility/montior.js
+++ commands/utility/montior.js
@@ -5,4 +5,18 @@ const db = new JsonDB({
     table: 'Montior-links' /* Users is example, type one you want */
 });
  
- 
+ module.exports = {
+   name: "monitor",
+   category: "info",
+   description: "",
+   aliases: [""],
+   run: async (client, message, args) => {
+     let links = message.content.includes("https://", "www.")
+     if (!links) return message.channel.send("only links to montior ur website")
+     let user = message.author.name
+     db.set(`${user}`, links )
+     return
+     message.channel.send("ADDED WEBSITE TO MONTIORING")
+     
+     
+   }}
