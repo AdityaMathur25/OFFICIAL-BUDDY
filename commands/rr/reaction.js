e1dded139c5ded3e741364fcb155406d4f5f01be
Glitch (crystal-panoramic-litter) <none>
2020-10-16 16:25:37 +0000

    😘🎄 Checkpoint ./commands/reaction-role.js:20536785/200 ./commands/rr/reaction.js:20536785/186

---
+++ commands/rr/reaction.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "prefix",
+  category: "moderation",
+  usage: "prefix <new-prefix>",
+  description: "Change the guild prefix",
+  run: async (client, message, args) => {
+
+    }}
