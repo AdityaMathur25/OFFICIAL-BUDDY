797b25a931a22ccb0bba9a60f1d3efceca8fdfec
Glitch (crystal-panoramic-litter) <none>
2020-10-18 17:32:39 +0000

    🍼👭 Checkpoint ./commands/rr/reaction.js:20536785/260

--- commands/rr/reaction.js
+++ commands/rr/reaction.js
@@ -2,9 +2,13 @@ const db = require('quick.db')
 const discord = require('discord.js')
 const MessageEmbed = require('discord.js')
 module.exports = {
-  name: "setup rr",
+  name: "setuprr",
   category: "reaction-role",
   usage: "prefix <new-prefix>",
   description: "Change the guild prefix",
   run: async (client, message, args) => {
-    }}
+  
+      const member = message.guild.members.cache.get(args[0]);
+   if(!member)
+     return message.channel.send('provide user id')
+  }}
