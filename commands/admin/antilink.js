d4ec2b52cfee2bfd7c0a30b7d89b6c7bd5bf1484
Glitch (crystal-panoramic-litter) <none>
2020-10-23 12:41:11 +0000

    ☕️🐮 Checkpoint ./commands/music/play.js:20536785/185 ./commands/admin/antilink.js:20536785/521

--- commands/admin/antilink.js
+++ commands/admin/antilink.js
@@ -16,35 +16,11 @@ module.exports = {
 
   run: (client, message, args) => {
 
-    
+    const  al = 
 
-    let dontt = message.mentions.channel.first//mentioned channel
+ 
 
-    
-
-   if(dontt > 5 ) {
-     return message.channel.send(" please mention channels ")
-     
-     }
-    if(!dontt) { //if channel is not mentioned
-
-      return message.channel.send("Please say enable to enable anti link")
-
-    }
-
-    
-
-    //Now we gonna use quick.db
-
-    
-
-    db.push(`anti_${message.guild.id}`, dontt.id) //set id in var
- db.push(`anti_${message.guild.id}`, dontt.id) //set id in var
-
-
-    if(dontt < 5) return
-
-    message.channel.send(`Anti-links  are now ${dontt}`) //send success message
+    message.channel.send(`Anti-links  are now $`) //send success message
 
   }
 
