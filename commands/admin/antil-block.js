2fe205d1a40933dbf60590c13c27356f95053e6e
Glitch (crystal-panoramic-litter) <none>
2020-10-24 06:36:29 +0000

    👲🍀 Checkpoint ./commands/admin/antil-block.js:20536785/3108

--- commands/admin/antil-block.js
+++ commands/admin/antil-block.js
@@ -1,20 +1,6 @@
-const D = require('easy-json-database')
-const db = new D("./black.json")
-module.exports = {
 
-  name: "blocklist",
 
-  category: "settings",
 
-  usage: "antilink block list @channel>",
 
-  description: "Set the welcome message, use {user} to ping new user , {server} to show server name , {members} to show member count",
 
-  aliases:["anl"],
-run: async (client, message, args ) => {
- const  m = message.mentions.channels.first();
-  if (m === null) return 
-  message.channel.send("mention channel first")
-  db.set(`${message.guild.id}`,`${m.id}`)
-  await message.channel.send(`${m} is now black-list channel for anti-links!`)
-}}
+  
