1dbf0e4b2395fc6201bded7b363f5d2c76c3fc15
Glitch (crystal-panoramic-litter) <none>
2020-10-30 13:01:19 +0000

    💕💴 Checkpoint ./commands/music/lyrics.js:13534914/62:20536785/36

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -17,32 +17,32 @@ module.exports = {
     
     const msg = await message.channel.send(embed)
      try {
-          const songs = await Genius.songs.search(args.join(" "));
+          const songs = await Genius.songs.search(args.join(" "), { limit: args.join(" ")});
           const lyrics = await songs[0].lyrics();
-          const image =  songs.thumbnail
+          const song = songs[0];
            if (lyrics.length > 4095) {
              msg.delete()
         return message.channel.send('Lyrics are too long to be returned as embed');
      }
       if (lyrics.length < 2048) {
         const lyricsEmbed = new MessageEmbed()
-        .setTitle(songs.title)
+        .setTitle(song.title)
           .setColor(COLOR)
-        .setThumbnail(songs.thumbnail)
+        .setThumbnail(song.thumbnail)
           .setDescription(lyrics.trim());
         
         return msg.edit(lyricsEmbed);
       } else {
         // lyrics.length > 2048
         const firstLyricsEmbed = new MessageEmbed()
-        .setTitle(songs.title)
+        .setTitle(song.title)
           .setColor(COLOR)
-        .setThumbnail(songs.thumbnail)
+        .setThumbnail(song.thumbnail)
           .setDescription(lyrics.slice(0, 2048));
         const secondLyricsEmbed = new MessageEmbed()
-        .setTitle(songs.title)
+        .setTitle(song.title)
           .setColor(COLOR)
-        .setThumbnail(songs.thumbnail)
+        .setThumbnail(song.thumbnail)
           .setDescription(lyrics.slice(2048, lyrics.length));
         msg.edit(firstLyricsEmbed);
         message.channel.send(secondLyricsEmbed);
