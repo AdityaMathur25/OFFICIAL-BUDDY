8209ba00860ed480174bfe5df2e3972bcc29a0e8
Glitch (crystal-panoramic-litter) <none>
2020-10-31 15:33:48 +0000

    🍠💨 Checkpoint ./events/guildMemberUpdate.js:20536785/371

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,9 +1,14 @@
 const db = require("quick.db")
 const { discord, MessageEmbed } = require("discord.js")
+const  { COLOR } = require("../config.json")
 module.exports.run = async(client, message, oldMember, newMember) => {
   let channel = await db.get(`logchannel_${message.guild.id}`)
  let avatar = new MessageEmbed()
- .setauthor(newMember.displayAvatarURL({dynamic: true }) + oldMember.username)
+ .setauthor(message.newMember.displayAvatarURL({dynamic: true }) + oldMember.username)
  .setdescription("CHANGE AVATAR ")
- 
+ .addfield("OLD AVATAR", `[OLD-AVATAR](${oldMember.displayAvatarURL()})`, true)
+ .setFooter("AVATAR LOGS! " + "FROM: " + message.guild.name)
+ .setTimestamp()
+ .setColor(COLOR)
+ client.channels.cache.get(channel).send(avatar)
   }
