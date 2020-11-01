635d120ead8038069d89d73c273ff666f0afdaf6
Glitch (crystal-panoramic-litter) <none>
2020-11-01 18:32:23 +0000

    🍦🕰 Checkpoint ./commands/music/skip.js:20536785/1726

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -7,13 +7,76 @@ module.exports = {
   category: "music",
     aliases: ["s"],
   
-  run: async function (client, message, args) {
-    const channel = message.member.voice.channel
-    if (!channel)return message.channel.send("I'm sorry but you need to be in a voice channel to play music!", message.channel);
-    const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue)return message.channel.send("There is nothing playing that I could skip for you.", message.channel);
-    serverQueue.connection.dispatcher.end("Skiped the music");
-    message.react("750762554418135151")
-    message.channel.send("Skipped the current song")
+  run: async function (client, msg, message, args) {
+    const channel = message.member.voiceChannel
+ const queue = client.queue.get(msg.guild.id);
+
+    if (!queue || !queue.playing)
+
+      return msg.channel.send(client.messages.noServerQueue);
+
+    if (msg.member.voiceChannel !== queue.voiceChannel)
+
+      return msg.channel.send(client.messages.wrongVoiceChannel);
+
+    vote(queue, msg, client);
+
   },
+
 };
+
+function skipSong(queue, msg, client) {
+
+  msg.channel.send(client.messages.skipped);
+
+  queue.endReason = "skip";
+
+  queue.time = 0;
+
+  queue.connection.dispatcher.end();
+
+}
+
+function vote(queue, msg, client) {
+
+  queue.votesNeeded = Math.floor(queue.voiceChannel.members.size / 2);
+
+  queue.votesNeeded.toFixed();
+
+  if (queue.voiceChannel.members.size > 2) {
+
+    if (queue.voters.includes(msg.member.id))
+
+      return msg.channel.send(client.messages.alreadyVoted);
+
+    queue.votes++;
+
+    queue.voters.push(msg.member.id);
+
+    if (queue.votes >= queue.votesNeeded) {
+
+      queue.voters = [];
+
+      queue.votes = 0;
+
+      queue.votesNeeded = null;
+
+      return skipSong(queue, msg, client);
+
+    } else
+
+      return msg.channel.send(
+
+        `${client.messages.notEnoughVotes} ${queue.votes} / ${queue.votesNeeded}!`
+
+      );
+
+  } else {
+
+    return skipSong(queue, msg, client);
+
+  }
+
+}
+    
+    
