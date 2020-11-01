a62605393f8d44ea8c99677bccb3fbfdb6aff98f
Glitch (crystal-panoramic-litter) <none>
2020-11-01 14:04:09 +0000

    🍚🎻 Checkpoint ./commands/support/afk.js:20536785/218

---
+++ commands/support/afk.js
@@ -0,0 +1,14 @@
+const db = require("quick.db")
+module.exports = {
+  name: "",
+  category: "",
+  description: "",
+  usage: "",
+  aliases: [""], 
+  run: async(client, message, args ) => {
+    
+    
+    
+    }} 
+  
+    
