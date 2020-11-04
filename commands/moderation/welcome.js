ace8f8cde086cae03a9a32953929a8ba9b37f2b7
Glitch (crystal-panoramic-litter) <none>
2020-11-04 13:20:13 +0000

    👙✍️ Checkpoint ./commands/moderation/welcome.js:19971311/537

--- commands/moderation/welcome.js
+++ commands/moderation/welcome.js
@@ -8,15 +8,21 @@ module.exports = {
   description: "Set the welcome channel",
   run: (client, message, args) => {
     
-    let channel = message.mentions.channels.first() //mentioned channel
+    let channel = message.mentions.channels.first()
+    let enable = args.join("").includes("enable")//mentioned channel
+  if(enable === enable.includes("disable")) {
+    }
     
     if(!channel) { //if channel is not mentioned
-      return message.channel.send("Please Mention the channel first")
+      return message.channel.send("Please Mention the channel first ")
     }
     
     //Now we gonna use quick.db
-    if()
-    db.set(`welchannel_${message.guild.id}`, channel.id) //set id in var
+   if(!enable){
+      return message.channel.send("type enable if u want to enable image else type disable")
+      }
+    
+    db.set(`welchannel_${message.guild.id}`, channel.id + enable) //set id in var
     
     message.channel.send(`Welcome Channel is seted as ${channel}`) //send success message
   }
