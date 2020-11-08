b00b964d10dd1d3142642e0013f7caf467233300
Glitch (cosmic-humorous-fall) <none>
2020-11-08 04:25:23 +0000

    🚆📟 Checkpoint ./commands/info/ping.js:20536785/1015 ./commands/images/sumg.js:20536785/601 ./package.json:20536785/3330 ./commands/image/trash.js:20536785/1042

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,8 +1,28 @@
+const discord = require("discord.js");
+
 module.exports = {
     name: "ping",
+    aliases: ["pong"],
     category: "info",
-    description: "Returns latency and API ping",
+    usage: "ping",
+    description: "Get the bot's ping!",
     run: async (client, message, args) => {
-       message.channel.send(`Pong - ${client.ws.ping}ms`)
+
+     let start = Date.now();
+  
+  message.channel.send({embed: {description: "Looks like the bot is slow.", color: "RANDOM"}}).then(m => {
+    
+    let end = Date.now();
+    
+    let embed = new discord.MessageEmbed()
+    .setAuthor("Ping!", message.author.avatarURL())
+    .setThumbnail(message.author.avatarURL({dynamic: true}))
+    .addField("API Latency", Math.round(client.ws.ping) + "ms", true)
+    .addField("Message Latency", end - start + "ms", true)
+    .setColor("RANDOM");
+    m.edit(embed).catch(e => message.channel.send(e))
+    
+  })
+
     }
-}
+};
