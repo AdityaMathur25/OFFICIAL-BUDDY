41a69940a664640575c6a997ae0c94f55e6c239a
Glitch (cosmic-humorous-fall) <none>
2020-11-08 09:40:18 +0000

    🏵😂 Checkpoint ./upvote.js:20536785/226

--- upvote.js
+++ upvote.js
@@ -4,17 +4,3 @@ const { Discord, message } = require('discord.js')
 
 
 
-//----- montioring -----
-const db = require("quick.db")
-const fetch = require("node-fetch") 
-let url = db.get(`url`)
-  
-setInterval(async () => {
-
-  await fetch(url).then(
-
-    console.log("website ping!")
-
-  );
-
-}, 3600000);
