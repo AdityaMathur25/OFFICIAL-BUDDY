2896ac74440e2ab50f585109479b3984e943715f
Glitch (cosmic-humorous-fall) <none>
2020-11-08 12:43:00 +0000

    🗽🚧 Checkpoint ./commands/admin/autorole.js:20536785/373

---
+++ commands/admin/autorole.js
@@ -0,0 +1,14 @@
+const  db = require("quick.db")
+module.exports = {
+name: "autorole",
+  category: "Administration",
+  description: "give role to new user In server",
+  alises: ["ar"],
+  usage: "ar enable/disabe role maximum 3 roles.",
+  run: async(client, message, args) =>{
+    let enabel =
+    
+    
+    
+  
+  }}
