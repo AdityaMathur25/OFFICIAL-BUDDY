066fa1552bbeef84212e492749a2322ec0cd005a
Glitch (cosmic-humorous-fall) <none>
2020-11-14 06:21:28 +0000

    🏩🐞 Checkpoint ./commands/info/level.js:20536785/802

--- commands/info/level.js
+++ commands/info/level.js
@@ -47,22 +47,7 @@ message.channel.send(attachment);
 return loadingMsg.delete();
 
 // XP Function
-
-function XP (message) {
-  if (message.content.startsWith(`cf!`)) return;
-  const randomNumber = Math.floor(Math.random() * 2) + 4;
-  db.add(`guild_${message.guild.id}_xp_${message.author.id}`, randomNumber);
-  db.add(`guild_${message.guild.id}_xptotal_${message.author.id}`, randomNumber)
-  var level = db.get(`guild_${message.guild.id}_level_${message.author.id}`) || 1;
-  var xp = db.get(`guild_${message.guild.id}_xp_${message.author.id}`);
-  var xpNeeded = level * 500;
-  if (xpNeeded < xp) {
-    var newLevel = db.add(`guild_${message.guild.id}_level_${message.author.id}`, 1)
-    db.subtract(`guild_${message.guild.id}_xp_${message.author.id}`, xpNeeded);
-    message.channel.send(`${message.author}, you have leveled up your campskills to level ${newLevel}!`)
-  }
 }
   }
-}
 
 
