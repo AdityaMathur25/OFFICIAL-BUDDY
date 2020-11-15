5c9d392bdd82ad7929a7b5b4748844a343cbe347
Glitch (cosmic-humorous-fall) <none>
2020-11-15 06:54:24 +0000

    🍴💓 Checkpoint ./commands/utility/afk.js:20536785/598 ./server.js:20536785/660

--- server.js
+++ server.js
@@ -294,6 +294,27 @@ client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction)
 
   }
   })
+if(db.has(`afk-${message.author.id}+${message.guild.id}`)) {
+
+        const info = db.get(`afk-${message.author.id}+${message.guild.id}`)
+
+         db.delete(`afk-${message.author.id}+${message.guild.id}`)
+
+        message.reply(`Your afk status have been removed (${info})`)
+
+    }
+
+    //checking for mentions
+
+    if(message.mentions.members.first()) {
+
+        if(db.has(`afk-${message.mentions.members.first().id}+${message.guild.id}`)) {
+
+            message.channel.send(message.mentions.members.first().user.tag + ":" + db.get(`afk-${message.mentions.members.first().id}+${message.guild.id}`))
+
+        }else;
+
+    }else;
 
 
 client.login(process.env.token);
